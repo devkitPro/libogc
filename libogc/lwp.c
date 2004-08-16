9ab345fee3be268e8cda7f092e8dc459573d98f0
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2004-08-16 17:01:36 +0000

    added some initializations in LWP_SleepThread

--- libogc/lwp.c
+++ libogc/lwp.c
@@ -173,7 +173,9 @@ u32 LWP_SleepThread(lwpq_t thequeue)
 	exec = _thr_executing;
 	_CPU_ISR_Disable(level);
 	__lwp_threadqueue_csenter(tqueue);
-	exec->wait.ret_code = 0;
+	exec->wait.ret_code = 1;
+	exec->wait.ret_arg = NULL;
+	exec->wait.ret_arg_1 = NULL;
 	exec->wait.queue = tqueue;
 	exec->wait.id = LWP_SLEEP_THREAD;
 	_CPU_ISR_Restore(level);
