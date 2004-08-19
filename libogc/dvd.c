ce3f8a4b9f062ac6cff0cd68d6b4adaec1affd85
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2004-08-19 06:29:31 +0000

    started to extend dvd functionality

--- libogc/dvd.c
+++ libogc/dvd.c
@@ -3,16 +3,32 @@
 #include "asm.h"
 #include "processor.h"
 #include "cache.h"
+#include "lwp.h"
+#include "irq.h"
 #include "dvd.h"
 
+#define DVD_BRK				(1<<0)
+#define DVD_DE_MSK			(1<<1)
+#define DVD_DE_INT			(1<<2)
+#define DVD_TC_MSK			(1<<3)
+#define DVD_TC_INT			(1<<4)
+#define DVD_BRK_MSK			(1<<5)
+#define DVD_BRK_INT			(1<<6)
+
 static u32 __DVDInitFlag = 0;
+static u32 __DVDStopNextInt = 0;
 static u32 __DVDNextCmdNum;
 static u32 __DVDWorkaround;
 static u32 __DVDWorkaroundSeek;
 
+static lwpq_t __dvd_wait_queue;
+
 static u32* const _diReg = (u32*)0xCC006000;
 
 extern void udelay(int us);
+
+extern void __MaskIrq(u32);
+extern void __UnmaskIrq(u32);
 
 static void __DVDLowWATypeSet(u32 workaround,u32 workaroundseek)
 {
@@ -32,6 +48,19 @@ static void __DVDInitWA()
 
 static void __DVDInterruptHandler(u32 nIrq,void *pCtx)
 {
+	u32 status,ir,imr,immr;
+
+	immr = 0;
+	status = _diReg[0];
+	imr = (status&(DVD_DE_MSK|DVD_TC_MSK|DVD_BRK_MSK));
+	ir = ((status&(DVD_DE_INT|DVD_TC_INT|DVD_BRK_INT))&(imr<<1));
+
+
+	if(ir&DVD_DE_INT) immr |= DVD_DE_INT;
+	if(ir&DVD_TC_INT) immr |= DVD_TC_INT;
+	if(ir&DVD_BRK_INT) immr |= DVD_BRK_INT;
+
+	_diReg[0] = (ir|imr);
 }
 
 s32 DVD_ReadId(void *pDst)
@@ -83,6 +112,16 @@ void DVD_Init()
 	if(__DVDInitFlag) return;
 	__DVDInitFlag = 1;
 
+	__DVDInitWA();
+
+	IRQ_Request(IRQ_PI_DI,__DVDInterruptHandler,NULL);
+	__UnmaskIrq(IRQMASK(IRQ_PI_DI));
+
+	LWP_InitQueue(&__dvd_wait_queue);
+
+	_diReg[0] = (DVD_DE_MSK|DVD_TC_MSK|DVD_BRK_MSK);
+	_diReg[1] = 0;
+	
 	if(!DVD_ReadId((void*)0x80000000)) return;
 	DCInvalidateRange((void*)0x80000000,0x20);
 
