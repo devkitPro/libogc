c28c5c9fecb3d8267267e9668f6a8062dd8e473e
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2005-01-09 02:08:36 +0000

    no message

--- gc/ogc/pad.h
+++ gc/ogc/pad.h
@@ -19,8 +19,8 @@
 #define PAD_BUTTON_DOWN				0x0004
 #define PAD_BUTTON_UP				0x0008
 #define PAD_TRIGGER_Z				0x0010
-#define PAD_TRIGGER_R				0x0020
-#define PAD_TRIGGER_L				0x0040
+#define PAD_TRIGGER_L				0x0020
+#define PAD_TRIGGER_R				0x0040
 #define PAD_BUTTON_A				0x0100
 #define PAD_BUTTON_B				0x0200
 #define PAD_BUTTON_X				0x0400
