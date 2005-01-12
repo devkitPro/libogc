d5d6d68e71f7ecf95431557c3f428a0c717f71fc
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2005-01-12 11:04:06 +0000

    no message

--- libsdcard/sdcard.c
+++ libsdcard/sdcard.c
@@ -16,105 +16,18 @@ struct _sd_file {
 	u32 size;
 	u8 mode;
 	u8 path[SDCARD_MAX_PATH_LEN];
+	F_HANDLE handle;
 	void *wbuffer;
 };
 
 static u32 sdcard_inited = 0;
 
 //#define _SDCARD_DEBUG
-/*
-static void __fill_csdregister(s32 chn)
+
+static s32 __sdcard_read(F_HANDLE handle,void *dest,u32 offset,u32 size)
 {
-	sdcard_block *card = NULL;
-	u8 *csd;
-
-	if(chn<EXI_CHANNEL_0 || chn>=EXI_CHANNEL_2) return;
-	card = &sdcard_map[chn];
-
-	csd = (u8*)card->csdh;
-	card->csd.csd_structure = (csd[0]>>6)&0x3;
-	card->csd.taac = csd[1];
-	card->csd.nasc = csd[2];
-	card->csd.tran_speed = csd[3];
-	card->csd.ccc = ((*(u16*)&(csd[4])>>4)&0x0fff);
-	card->csd.read_bl_len = csd[5]&0x0f;
-	card->csd.read_bl_part = (csd[6]>>7)&0x01;
-	card->csd.write_bl_misal = (csd[6]>>6)&0x01;
-	card->csd.read_bl_misal = (csd[6]>>5)&0x01;
-	card->csd.dsr_imp = (csd[6]>>4)&0x01;
-	card->csd.c_size = (u32)(((csd[6]&0x03)<<10)|(csd[7]<<2)|((csd[8]>>6)&0x03));
-	card->csd.vdd_r_curr_min = (csd[8]>>3)&0x07;
-	card->csd.vdd_r_curr_max = csd[8]&0x07;
-	card->csd.vdd_w_curr_min = (csd[9]>>5)&0x07;
-	card->csd.vdd_w_curr_max = (csd[9]>>2)&0x07;
-	card->csd.c_size_mult = ((csd[9]&0x03)<<1)|((csd[10]>>7)&0x01);
-	card->csd.erase_bl_en = (csd[10]>>6)&0x01;
-	card->csd.sector_size = ((csd[10]&0x3f)<<1)|((csd[11]>>1)&0x01);
-	card->csd.wp_grp_size = csd[11]&0x7f;
-	card->csd.wp_grp_en = (csd[12]>>7)&0x01;
-	card->csd.r2w_fact = (csd[12]>>2)&0x07;
-	card->csd.write_bl_len = ((csd[12]&0x03)<<2)|((csd[13]>>6)&0x03);
-	card->csd.write_bl_part = (csd[13]>>5)&0x01;
-	card->csd.file_fmt_grp = (csd[14]>>7)&0x01;
-	card->csd.copy = (csd[14]>>6)&0x01;
-	card->csd.perm_write_prot = (csd[14]>>5)&0x01;
-	card->csd.tmp_write_prot = (csd[14]>>4)&0x01;
-	card->csd.file_fmt = (csd[14]>>2)&0x03;
-}
-
-static void __fill_cidregister(s32 chn)
-{
-	sdcard_block *card = NULL;
-	u8 *cid;
-
-	if(chn<EXI_CHANNEL_0 || chn>=EXI_CHANNEL_2) return;
-	card = &sdcard_map[chn];
-
-	cid = (u8*)card->cidh;
-	card->cid.mid = cid[0];
-	card->cid.oid[0] = cid[1];
-	card->cid.oid[1] = cid[2];
-	card->cid.prv = cid[8];
-	card->cid.psn = *(u32*)&(cid[9]);
-	card->cid.mdt = (*(u16*)&(cid[13])&0x0fff);
-	memcpy(card->cid.pnm,cid+3,5);
-}
-
-static s32 __sdcard_writeD(s32 chn,void *buffer,u32 blocks,u32 sector)
-{
-	u8 *ptr;
-	s32 ret,cnt;
-	sdcard_block *card = NULL;
-
-	if(chn<EXI_CHANNEL_0 || chn>=EXI_CHANNEL_2) return SDCARD_ERROR_NOCARD;
-	card = &sdcard_map[chn];
-
-	if((ret=__sdcard_sendcmd(chn,0x19,(sector*card->sector_size)))!=0) return ret;
-	if((ret=__sdcard_response1(chn))==0) {
-		cnt = 0;
-		ptr = buffer;
-		while(cnt<blocks) {
-			if((ret=__sdcard_multidatawrite(chn,ptr,card->sector_size))!=0) break;
-			ptr += card->sector_size;
-			cnt++;
-		}
-	}
-	
-	return ret;
-
-
-	return SDCARD_ERROR_READY;
-}
-*/
-
-static s32 __sdcard_read(const char *filename,void *dest,u32 offset,u32 size)
-{
-	u32 oflags = 1;
 	u32 read_cnt = 0;
-	F_HANDLE handle;
 	
-	if(card_openFile(filename,oflags,&handle)!=0) return 0;
-
 	if(offset>0) {
 		u32 sd_offset = -1;
 		u32 mode = 1;
@@ -122,8 +35,6 @@ static s32 __sdcard_read(const char *filename,void *dest,u32 offset,u32 size)
 	}
 
 	card_readFile(handle,dest,size,&read_cnt);
-	card_closeFile(handle);
-
 	return read_cnt;
 }
 
@@ -150,31 +61,33 @@ s32 SDCARD_Init()
 sd_file* SDCARD_OpenFile(const char *filename,const char *mode)
 {
 	s32 err;
-	u32 size;
-	u8 buffer[8];
 	struct _sd_file *rfile = NULL;
 	
 	rfile = (struct _sd_file*)__lwp_wkspace_allocate(sizeof(struct _sd_file));
 	if(!rfile)
 		return NULL;
 
+	rfile->handle = -1;
 	if(mode[0]=='r') {
-		err = __sdcard_read(filename,buffer,0,1);
+		err = card_openFile(filename,OPEN_R,&rfile->handle);
 		if(err==1) {
-			card_getFileSize(filename,&size);
+			card_getFileSize(filename,&rfile->size);
 			strncpy(rfile->path,filename,SDCARD_MAX_PATH_LEN);
 			rfile->pos = 0;
 			rfile->mode = 'r';
-			rfile->size = size;	
+			rfile->wbuffer = NULL;
 			return (sd_file*)rfile;
 		}
 	}
 	if(mode[0]=='w') {
-		strncpy(rfile->path,filename,SDCARD_MAX_PATH_LEN);
-		rfile->pos = 0;
-		rfile->size = 0;
-		rfile->mode = 'w';
-		rfile->wbuffer = NULL;
+		err = card_openFile(filename,OPEN_W,&rfile->handle);
+		if(err==1) {
+			strncpy(rfile->path,filename,SDCARD_MAX_PATH_LEN);
+			rfile->pos = 0;
+			rfile->size = 0;
+			rfile->mode = 'w';
+			rfile->wbuffer = NULL;
+		}
 		return (sd_file*)rfile;
 	}
 	__lwp_wkspace_free(rfile);
@@ -187,8 +100,8 @@ s32 SDCARD_ReadFile(sd_file *pfile,void *buf,u32 len)
 	u8 *buffer = (u8*)buf;
 	struct _sd_file *ifile = (struct _sd_file*)pfile;
 
-	if(ifile && ifile->mode=='r') {
-		ret = __sdcard_read(ifile->path,buffer,ifile->pos,len);
+	if(ifile && ifile->mode=='r' && ifile->handle!=-1) {
+		ret = __sdcard_read(ifile->handle,buffer,ifile->pos,len);
 		ifile->pos += len;
 	}
 	return ret;
@@ -246,8 +159,12 @@ s32 SDCARD_ReadDir(const char *dirname,DIR *pdir_list)
 s32 SDCARD_CloseFile(sd_file *pfile)
 {
 	struct _sd_file *ifile = (struct _sd_file*)pfile;
-	if(ifile) {
-		if(ifile->mode=='w') {
+
+	if(ifile && ifile->handle!=-1) {
+		if(ifile->mode=='r') {
+			card_closeFile(ifile->handle);
+		}
+		else if(ifile->mode=='w') {
 		}
 		__lwp_wkspace_free(ifile);
 	}
