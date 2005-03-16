e0bdc0b4c1930bc0b7a5c682120cdc82d93c8e7c
Dave Murphy <wntrmute@users.sourceforge.net>
2005-03-16 20:55:14 +0000

    added pad button reading API

--- libogc/pad.c
+++ libogc/pad.c
@@ -529,3 +529,54 @@ void PAD_ControlMotor(s32 chan,u32 cmd)
 	}
 	_CPU_ISR_Restore(level);
 }
+
+//---------------------------------------------------------------------------------
+typedef struct tagKeyInput{
+	vs16	up,
+			down,
+			state,
+			oldstate;
+	}__attribute__ ((packed)) KeyInput;
+
+static volatile KeyInput Keys[4] = { {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+static PADStatus pad[4];
+
+
+//---------------------------------------------------------------------------------
+void PAD_ScanPads() {
+//---------------------------------------------------------------------------------
+
+	PAD_Read(pad);
+	int index;
+
+	for ( index = 0; index < 4; index++) {
+
+		Keys[index].oldstate	= Keys[index].state;
+		Keys[index].state		= pad[index].button;
+		Keys[index].up			= Keys[index].oldstate & ~Keys[index].state;
+		Keys[index].down		= ~Keys[index].oldstate & Keys[index].state;
+	}			
+}
+
+//---------------------------------------------------------------------------------
+u16 PAD_ButtonsUp(int pad) {
+//---------------------------------------------------------------------------------
+	if (pad<0 || pad >3) return 0;
+	
+	return 	Keys[pad].up;
+}
+//---------------------------------------------------------------------------------
+u16 PAD_ButtonsDown(int pad) {
+//---------------------------------------------------------------------------------
+	if (pad<0 || pad >3) return 0;
+
+	return 	Keys[pad].down;
+}
+
+//---------------------------------------------------------------------------------
+u16 PAD_ButtonsHeld(int pad) {
+//---------------------------------------------------------------------------------
+	if (pad<0 || pad >3) return 0;
+
+	return Keys[pad].state;
+}
