65c94339351ff8d9d93635e4b2fa20c9252e100b
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2005-11-22 11:19:33 +0000

    - adjusted doxygen documentation style in file.

--- gc/ogc/audio.h
+++ gc/ogc/audio.h
@@ -1,6 +1,6 @@
 /*-------------------------------------------------------------
 
-$Id: audio.h,v 1.4 2005-11-21 10:51:21 shagkur Exp $
+$Id: audio.h,v 1.5 2005-11-22 11:19:33 shagkur Exp $
 
 audio.h -- Audio subsystem
 
@@ -28,6 +28,9 @@ must not be misrepresented as being the original software.
 distribution.
 
 $Log: not supported by cvs2svn $
+Revision 1.4  2005/11/21 10:51:21  shagkur
+no message
+
 
 -------------------------------------------------------------*/
 
@@ -72,203 +75,246 @@ $Log: not supported by cvs2svn $
    extern "C" {
 #endif /* __cplusplus */
 
-/*! \typedef void (*AIDCallback)(void)
-\brief function pointer typedef for the user's Audio DMA interrupt callback
-\param none
-*/
+/*! 
+ * \typedef void (*AIDCallback)(void)
+ * \brief function pointer typedef for the user's Audio DMA interrupt callback
+ *
+ * \param none
+ */
 typedef void (*AIDCallback)(void);
 
 
-/*! \typedef void (*AISCallback)(u32 smp_cnt)
-\brief function pointer typedef for the user's Audio Streaming interrupt callback
-\param smp_cnt AI sample count
-*/
+/*! 
+ * \typedef void (*AISCallback)(u32 smp_cnt)
+ * \brief function pointer typedef for the user's Audio Streaming interrupt callback
+ *
+ * \param smp_cnt AI sample count
+ */
 typedef void (*AISCallback)(u32 smp_cnt);
 
 
-/*! \fn AISCallback AUDIO_RegisterStreamCallback(AISCallback callback)
-\brief Register a callback function for the DVD audiostreaming interface
-\param[in] callback pointer to the function which to call when AIS interrupt has triggered.
-
-\return pointer to old callback function or NULL respectively.
-*/
+/*! 
+ * \fn AISCallback AUDIO_RegisterStreamCallback(AISCallback callback)
+ * \brief Register a user callback function for the AUDIO streaming interface
+ *
+ * \param[in] callback pointer to the function which to call when AIS interrupt has triggered.
+ *
+ * \return pointer to old callback function or NULL respectively.
+ */
 AISCallback AUDIO_RegisterStreamCallback(AISCallback callback);
 
 
-/*! \fn void AUDIO_Init(u8 *stack)
-\brief Initialize the AUDIO subsystem
-\param[in] stack pointer to a memory area to work as stack when calling the callbacks. May be NULL
-
-\return none
-*/
+/*! 
+ * \fn void AUDIO_Init(u8 *stack)
+ * \brief Initialize the AUDIO subsystem
+ *
+ * \param[in] stack pointer to a memory area to work as stack when calling the callbacks. May be NULL
+ *
+ * \return none
+ */
 void AUDIO_Init(u8 *stack);
 
-/*! \fn void AUDIO_SetStreamVolLeft(u8 vol)
-\brief Set streaming volume on the left channel.
-\param[in] vol level of volume 0<= vol <=255
 
-\return none
-*/
+/*! 
+ * \fn void AUDIO_SetStreamVolLeft(u8 vol)
+ * \brief Set streaming volume on the left channel.
+ *
+ * \param[in] vol level of volume 0<= vol <=255
+ *
+ * \return none
+ */
 void AUDIO_SetStreamVolLeft(u8 vol);
 
 
-/*! \fn u8 AUDIO_GetStreamVolLeft()
-\brief Get streaming volume of the left channel.
-
-\return level of volume.
-*/
+/*! 
+ * \fn u8 AUDIO_GetStreamVolLeft()
+ * \brief Get streaming volume of the left channel.
+ *
+ * \return level of volume.
+ */
 u8 AUDIO_GetStreamVolLeft();
 
 
-/*! \fn void AUDIO_SetStreamVolRight(u8 vol)
-\brief set streaming volume of the right channel.
-\param[in] vol level of volume 0<= vol <=255
-
-\return none
-*/
+/*! 
+ * \fn void AUDIO_SetStreamVolRight(u8 vol)
+ * \brief set streaming volume of the right channel.
+ *
+ * \param[in] vol level of volume 0<= vol <=255
+ *
+ * \return none
+ */
 void AUDIO_SetStreamVolRight(u8 vol);
 
 
-/*! \fn u8 AUDIO_GetStreamVolRight()
-\brief Get streaming volume of the right channel.
-
-\return level of volume.
-*/
+/*! 
+ * \fn u8 AUDIO_GetStreamVolRight()
+ * \brief Get streaming volume of the right channel.
+ *
+ * \return level of volume.
+ */
 u8 AUDIO_GetStreamVolRight();
 
 
-/*! \fn void AUDIO_SetStreamSampleRate(u32 rate)
-\brief Set streaming sample rate
-\param[in] rate streaming sample rate (AI_SAMPLERATE_32KHZ,AI_SAMPLERATE_48KHZ)
-
-\return none
-*/
+/*! 
+ * \fn void AUDIO_SetStreamSampleRate(u32 rate)
+ * \brief Set streaming sample rate
+ *
+ * \param[in] rate streaming \ref ai_sample_rates "sample rate"
+ *
+ * \return none
+ */
 void AUDIO_SetStreamSampleRate(u32 rate);
 
 
-/*! \fn u32 AUDIO_GetStreamSampleRate()
-\brief Get streaming sample rate
-
-\return streaming sample rate (AI_SAMPLERATE_32KHZ,AI_SAMPLERATE_48KHZ)
-*/
+/*! 
+ * \fn u32 AUDIO_GetStreamSampleRate()
+ * \brief Get streaming sample rate
+ *
+ * \return \ref ai_sample_rates "sample rate"
+ */
 u32 AUDIO_GetStreamSampleRate();
 
 
-/*! \fn AIDCallback AUDIO_RegisterDMACallback(AIDCallback callback)
-\brief Register a callback function for the audio DMA interface.
-\param[in] callback pointer to the function which to call when AID interrupt has triggered.
-
-\return pointer to old callback function or NULL respectively.
-*/
+/*! 
+ * \fn AIDCallback AUDIO_RegisterDMACallback(AIDCallback callback)
+ * \brief Register a user callback function for the audio DMA interface.
+ *
+ *        This callback will be called whenever the audio DMA requests new data.<br>
+ *        Internally the DMA buffers are double buffered.
+ *
+ * \param[in] callback pointer to the function which to call when AID interrupt has triggered.
+ *
+ * \return pointer to old callback function or NULL respectively.
+ */
 AIDCallback AUDIO_RegisterDMACallback(AIDCallback callback);
 
 
-/*! \fn void AUDIO_InitDMA(u32 startaddr,u32 len)
-\brief Initialize an audio DMA transfer
-\param[in] startaddr start address of the memory region to load into the audio DMA. NOTE: Has to be aligned on a 32byte boundery!
-\param[in] len lenght of data to load into the audio DMA.
-
-\return none
-*/
+/*! 
+ * \fn void AUDIO_InitDMA(u32 startaddr,u32 len)
+ * \brief Initialize an audio DMA transfer
+ *
+ * \param[in] startaddr start address of the memory region to load into the audio DMA. <b><i>NOTE:</i></b> Has to be aligned on a 32byte boundery!
+ * \param[in] len lenght of data to load into the audio DMA. <b><i>NOTE:</i></b> Sould be a multiple of 32
+ *
+ * \return none
+ */
 void AUDIO_InitDMA(u32 startaddr,u32 len);
 
 
-/*! \fn u16 AUDIO_GetDMAEnableFlag()
-\brief Get the audio DMA flag
-
-\return state of the current DMA operation.
-*/
+/*! 
+ * \fn u16 AUDIO_GetDMAEnableFlag()
+ * \brief Get the audio DMA flag
+ *
+ * \return state of the current DMA operation.
+ */
 u16 AUDIO_GetDMAEnableFlag();
 
 
-/*! \fn void AUDIO_StartDMA()
-\brief Start the, previously with AUDIO_InitDMA() initialized, audio DMA operation.
-
-\return none
-*/
+/*! 
+ * \fn void AUDIO_StartDMA()
+ * \brief Start the audio DMA operation.
+ *
+ *        Starts to transfer the data from main memory to the audio interface thru DMA.<br>
+ *        This call should follow the call to AUDIO_InitDMA() which is used to initialize DMA transfers. 
+ *
+ * \return none
+ */
 void AUDIO_StartDMA();
 
 
-/*! \fn void AUDIO_StopDMA()
-\brief Stop the previously started audio DMA operation.
-
-\return none
-*/
+/*! 
+ * \fn void AUDIO_StopDMA()
+ * \brief Stop the previously started audio DMA operation.
+ *
+ * \return none
+ */
 void AUDIO_StopDMA();
 
 
-/*! \fn u32 AUDIO_GetDMABytesLeft()
-\brief Get the count of bytes, left to play, from the audio DMA interface
-
-\return count of bytes left to play.
-*/
+/*!
+ * \fn u32 AUDIO_GetDMABytesLeft()
+ * \brief Get the count of bytes, left to play, from the audio DMA interface
+ *
+ * \return count of bytes left to play.
+ */
 u32 AUDIO_GetDMABytesLeft();
 
 
-/*! \fn u32 AUDIO_GetDMALength()
-\brief Get the DMA transfer length configured in the audio DMA interface.
-
-\return length of data loaded into the audio DMA interface.
-*/
+/*! 
+ * \fn u32 AUDIO_GetDMALength()
+ * \brief Get the DMA transfer length configured in the audio DMA interface.
+ * 
+ * \return length of data loaded into the audio DMA interface.
+ */
 u32 AUDIO_GetDMALength();
 
 
-/*! \fn u32 AUDIO_GetDMAStartAddr()
-\brief Get the main memory address for the DMA operation.
-
-\return start address of mainmemory loaded into the audio DMA interface.
-*/
+/*! 
+ * \fn u32 AUDIO_GetDMAStartAddr()
+ * \brief Get the main memory address for the DMA operation.
+ *
+ * \return start address of mainmemory loaded into the audio DMA interface.
+ */
 u32 AUDIO_GetDMAStartAddr();
 
 
-/*! \fn void AUDIO_SetStreamTrigger(u32 cnt)
-\brief Set the sample count for the stream trigger
-\param[in] cnt count of samples when to trigger the audio stream.
-
-\return none
-*/
+/*! 
+ * \fn void AUDIO_SetStreamTrigger(u32 cnt)
+ * \brief Set the sample count for the stream trigger
+ *
+ * \param[in] cnt count of samples when to trigger the audio stream.
+ *
+ * \return none
+ */
 void AUDIO_SetStreamTrigger(u32 cnt);
 
 
-/*! \fn void AUDIO_ResetStreamSampleCnt()
-\brief Reset the stream sample count register.
-
-\return none
-*/
+/*! 
+ * \fn void AUDIO_ResetStreamSampleCnt()
+ * \brief Reset the stream sample count register.
+ *
+ * \return none
+ */
 void AUDIO_ResetStreamSampleCnt();
 
 
-/*! \fn void AUDIO_SetDSPSampleRate(u8 rate)
-\brief Set the sampling rate for the DSP interface
-\param[in] rate sampling rate to set for the DSP (AI_SAMPLERATE_32KHZ,AI_SAMPLERATE_48KHZ)
-
-\return none
-*/
+/*! 
+ * \fn void AUDIO_SetDSPSampleRate(u8 rate)
+ * \brief Set the sampling rate for the DSP interface
+ *
+ * \param[in] rate sampling rate to set for the DSP (AI_SAMPLERATE_32KHZ,AI_SAMPLERATE_48KHZ)
+ *
+ * \return none
+ */
 void AUDIO_SetDSPSampleRate(u8 rate);
 
 
-/*! \fn u32 AUDIO_GetDSPSampleRate()
-\brief Get the sampling rate for the DSP interface
-
-\return DSP sampling rate (AI_SAMPLERATE_32KHZ,AI_SAMPLERATE_48KHZ)
-*/
+/*! 
+ * \fn u32 AUDIO_GetDSPSampleRate()
+ * \brief Get the sampling rate for the DSP interface
+ *
+ * \return DSP sampling rate (AI_SAMPLERATE_32KHZ,AI_SAMPLERATE_48KHZ)
+ */
 u32 AUDIO_GetDSPSampleRate();
 
 
-/*! \fn void AUDIO_SetStreamPlayState(u32 state)
-\brief Set the play state for the streaming audio interface
-\param[in] state playstate of the streaming audio interface (AI_STREAM_STOP,AI_STREAM_START)
-
-\return none
-*/
+/*! 
+ * \fn void AUDIO_SetStreamPlayState(u32 state)
+ * \brief Set the play state for the streaming audio interface
+ *
+ * \param[in] state playstate of the streaming audio interface (AI_STREAM_STOP,AI_STREAM_START)
+ *
+ * \return none
+ */
 void AUDIO_SetStreamPlayState(u32 state);
 
 
-/*! \fn u32 AUDIO_GetStreamPlayState()
-\brief Get the play state from the streaming audio interface
-
-\return playstate (AI_STREAM_STOP,AI_STREAM_START)
-*/
+/*! 
+ * \fn u32 AUDIO_GetStreamPlayState()
+ * \brief Get the play state from the streaming audio interface
+ *
+ * \return playstate (AI_STREAM_STOP,AI_STREAM_START)
+ */
 u32 AUDIO_GetStreamPlayState();
 
 #ifdef __cplusplus
