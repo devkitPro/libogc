165f90cf2bf289a2344c13769cabfc6b09cff7af
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2005-11-22 14:05:42 +0000

    - added copyright header (taken from libnds) - introduced CVS ID and LOG token - started doxygen styled documentation

--- gc/ogc/aram.h
+++ gc/ogc/aram.h
@@ -1,6 +1,6 @@
 /*-------------------------------------------------------------
 
-$Id: aram.h,v 1.4 2005-11-21 10:49:01 shagkur Exp $
+$Id: aram.h,v 1.5 2005-11-22 14:05:42 shagkur Exp $
 
 aram.h -- ARAM subsystem
 
@@ -28,6 +28,9 @@ must not be misrepresented as being the original software.
 distribution.
 
 $Log: not supported by cvs2svn $
+Revision 1.4  2005/11/21 10:49:01  shagkur
+no message
+
 
 -------------------------------------------------------------*/
 
@@ -35,25 +38,31 @@ $Log: not supported by cvs2svn $
 #ifndef __ARAM_H__
 #define __ARAM_H__
 
-/*! \file aram.h 
-\brief ARAM subsystem
-
-*/ 
+/*! 
+ * \file aram.h 
+ * \brief ARAM subsystem
+ *
+ */ 
 
 #include <gctypes.h>
 
 
-/*! \addtogroup dmamode ARAM DMA transfer direction
+/*! 
+ * \addtogroup dmamode ARAM DMA transfer direction
  * @{
  */
 
 #define AR_MRAMTOARAM		0		/*!< direction: MRAM -> ARAM (write) */
 #define AR_ARAMTOMRAM		1		/*!< direction: ARAM -> MRAM (read) */
 
-/*! @} */
+/*!
+ * @}
+ */
 
 
-/*! \addtogroup memmode ARAM memory access modes
+
+/*!
+ * \addtogroup memmode ARAM memory access modes
  * @{
  */
 
@@ -61,7 +70,9 @@ $Log: not supported by cvs2svn $
 #define AR_ARAMINTUSER		1		/*!< use only the internal user space of the ARAM memory */
 #define AR_ARAMEXPANSION	2		/*!< use expansion ARAM memory, if present */
 
-/*! @} */
+/*!
+ * @}
+ */
 
 
 #ifdef __cplusplus
@@ -69,129 +80,151 @@ $Log: not supported by cvs2svn $
 #endif /* __cplusplus */
 
 
-/*! \typedef void (*ARCallback)(void)
-\brief function pointer typedef for the user's ARAM interrupt callback
-\param none
-*/
+/*! 
+ * \typedef void (*ARCallback)(void)
+ * \brief function pointer typedef for the user's ARAM interrupt callback
+ *
+ * \param none
+ */
 typedef void (*ARCallback)(void);
 
 
-/*! \fn ARCallback AR_RegisterCallback(ARCallback callback)
-\brief Register a user callback function for the ARAM interrupt handler.
-\param[in] callback pointer to callback to call when ARAM interrupt occured.
-
-\return pointer to old callback function or NULL respectively.
-*/
+/*! 
+ * \fn ARCallback AR_RegisterCallback(ARCallback callback)
+ * \brief Register a user callback function for the ARAM interrupt handler.
+ *
+ * \param[in] callback pointer to callback to call when ARAM interrupt occured.
+ *
+ * \return pointer to old callback function or NULL respectively.
+ */
 ARCallback AR_RegisterCallback(ARCallback callback);
 
 
-/*! \fn u32 AR_GetDMAStatus()
-\brief Get ARAM DMA status
-
-\return DMA status
-*/
+/*! 
+ * \fn u32 AR_GetDMAStatus()
+ * \brief Get ARAM DMA status
+ *
+ * \return DMA status
+ */
 u32 AR_GetDMAStatus();
 
 
-/*! \fn u32 AR_Init(u32 *stack_idx_array,u32 num_entries)
-\brief Initializes ARAM subsystem.
-\param[in] stack_idx_array pointer to an array of indicies to keep track on allocated ARAM blocks.
-\param[in] num_entries maximum count of entries the array can take.
-
-\return ARAM baseaddress
-*/
+/*! 
+ * \fn u32 AR_Init(u32 *stack_idx_array,u32 num_entries)
+ * \brief Initializes ARAM subsystem.
+ *
+ * \param[in] stack_idx_array pointer to an array of indicies to keep track on allocated ARAM blocks.
+ * \param[in] num_entries maximum count of entries the array can take.
+ *
+ * \return ARAM baseaddress
+ */
 u32 AR_Init(u32 *stack_idx_array,u32 num_entries);
 
 
-/*! \fn void AR_StartDMA(u32 dir,u32 memaddr,u32 aramaddr,u32 len)
-\brief Initialize and start ARAM DMA transfer.
-\param[in] dir direction of \ref dmamode "ARAM DMA operation"
-\param[in] memaddr main memory address to read/write from/to data.
-\param[in] aramaddr ARAM memory address to write/read to/from data.
-\param[in] len length of data to copy.
-
-\return none
-*/
+/*! 
+ * \fn void AR_StartDMA(u32 dir,u32 memaddr,u32 aramaddr,u32 len)
+ * \brief Initialize and start ARAM DMA transfer.
+ *
+ * \param[in] dir direction of \ref dmamode "ARAM DMA operation"
+ * \param[in] memaddr main memory address to read/write from/to data.
+ * \param[in] aramaddr ARAM memory address to write/read to/from data.
+ * \param[in] len length of data to copy.
+ *
+ * \return none
+ */
 void AR_StartDMA(u32 dir,u32 memaddr,u32 aramaddr,u32 len);
 
 
-/*! \fn u32 AR_Alloc(u32 len)
-\brief Allocate a block from ARAM
-\param[in] len length of block to allocate.
-
-\return ARAM blockstartaddress
-*/
+/*! 
+ * \fn u32 AR_Alloc(u32 len)
+ * \brief Allocate a block from ARAM
+ *
+ * \param[in] len length of block to allocate.
+ *
+ * \return ARAM blockstartaddress
+ */
 u32 AR_Alloc(u32 len);
 
 
-/*! \fn u32 AR_Free(u32 *len)
-\brief Free a block from ARAM
-\param[out] len pointer to receive the length of the free'd ARAM block. This is optional and can be NULL.
-
-\return ARAM current baseaddress after free'ing the block
-*/
+/*! 
+ * \fn u32 AR_Free(u32 *len)
+ * \brief Free a block from ARAM
+ *
+ * \param[out] len pointer to receive the length of the free'd ARAM block. This is optional and can be NULL.
+ *
+ * \return ARAM current baseaddress after free'ing the block
+ */
 u32 AR_Free(u32 *len);
 
 
-/*! \fn void AR_Clear(u32 flag)
-\brief Clear ARAM memory
-\param[in] flag Mode flag for clear operation (ARAMINTALL, ARAMINTUSER, ARAMEXPANSION)
-
-\return none
-*/
+/*!
+ * \fn void AR_Clear(u32 flag)
+ * \brief Clear ARAM memory
+ *
+ * \param[in] flag Mode flag for clear operation (ARAMINTALL, ARAMINTUSER, ARAMEXPANSION)
+ *
+ * \return none
+ */
 void AR_Clear(u32 flag);
 
 
-/*! \fn BOOL AR_CheckInit()
-\brief Check if ARAM subsystem is initialized.
-
-\return boolean value
-*/
+/*! 
+ * \fn BOOL AR_CheckInit()
+ * \brief Check if ARAM subsystem is initialized.
+ *
+ * \return boolean value
+ */
 BOOL AR_CheckInit();
 
 
-/*! \fn void AR_Reset()
-\brief Reset the ARAM subsystem initialization flag
-
-\return none
-*/
+/*!
+ * \fn void AR_Reset()
+ * \brief Reset the ARAM subsystem initialization flag
+ *
+ * \return none
+ */
 void AR_Reset();
 
 
-/*! \fn u32 AR_GetSize()
-\brief Get the whole size, including expansion, of ARAM memory
-
-\return ARAM memory size
-*/
+/*! 
+ * \fn u32 AR_GetSize()
+ * \brief Get the whole size, including expansion, of ARAM memory
+ *
+ * \return ARAM memory size
+ */
 u32 AR_GetSize();
 
 
-/*! \fn u32 AR_GetBaseAddress()
-\brief Get the baseaddress of ARAM memory
-
-\return ARAM memory baseaddress
-*/
+/*! 
+ * \fn u32 AR_GetBaseAddress()
+ * \brief Get the baseaddress of ARAM memory
+ *
+ * \return ARAM memory baseaddress
+ */
 u32 AR_GetBaseAddress();
 
 
-/*! \fn u32 AR_GetInternalSize()
-\brief Get the size of the internal ARAM memory
-
-\return ARAM internal memory size
-*/
+/*! 
+ * \fn u32 AR_GetInternalSize()
+ * \brief Get the size of the internal ARAM memory
+ *
+ * \return ARAM internal memory size
+ */
 u32 AR_GetInternalSize();
 
 
-/*! \def AR_StartDMARead(maddr,araddr,tlen)
-\brief Wraps the DMA read operation done by AR_StartDMA()
-*/
+/*! 
+ * \def AR_StartDMARead(maddr,araddr,tlen)
+ * \brief Wraps the DMA read operation done by AR_StartDMA()
+ */
 #define AR_StartDMARead(maddr,araddr,tlen)	\
 	AR_StartDMA(AR_ARAMTOMRAM,maddr,araddr,tlen);
 
 
-/*! \def AR_StartDMAWrite(maddr,araddr,tlen)
-\brief Wraps the DMA write operation done by AR_StartDMA()
-*/
+/*!
+ * \def AR_StartDMAWrite(maddr,araddr,tlen)
+ * \brief Wraps the DMA write operation done by AR_StartDMA()
+ */
 #define AR_StartDMAWrite(maddr,araddr,tlen)	\
 	AR_StartDMA(AR_MRAMTOARAM,maddr,araddr,tlen);
 
