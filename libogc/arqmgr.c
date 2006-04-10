e8981cf44c2fba255098d05d5e40f6bb6782f253
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2006-04-10 05:25:41 +0000

    - changed task queueing - implemented a/synchronus variants of ARQ_PostRequest

--- libogc/arqmgr.c
+++ libogc/arqmgr.c
@@ -1,6 +1,6 @@
 /*-------------------------------------------------------------
 
-$Id: arqmgr.c,v 1.8 2005-12-09 09:35:45 shagkur Exp $
+$Id: arqmgr.c,v 1.9 2006-04-10 05:25:41 shagkur Exp $
 
 arqmgr.c -- ARAM task request queue management
 
@@ -28,6 +28,9 @@ must not be misrepresented as being the original software.
 distribution.
 
 $Log: not supported by cvs2svn $
+Revision 1.8  2005/12/09 09:35:45  shagkur
+no message
+
 Revision 1.7  2005/11/22 13:53:46  shagkur
 no message
 
@@ -50,79 +53,120 @@ Revision 1.5  2005/11/21 12:42:30  shagkur
 
 #define ARQM_STACKENTRIES		16
 #define ARQM_ZEROBYTES			256
+#define ROUNDUP32(x)			(((u32)(x)+0x1f)&~0x1f)
 
 typedef struct _arqm_info {
 	ARQRequest arqhandle;
 	ARQMCallback callback;
-	void *buff;
-	u32 len;
+	void *buffer;
+	u32 file_len;
+	u32 read_len;
 	u32 aram_start;
+	u32 curr_read_offset;
 	u32 curr_aram_offset;
-	volatile BOOL poll_flag;
+	volatile BOOL polled;
 } ARQM_Info;
 
 static u32 __ARQMStackLocation;
 static u32 __ARQMFreeBytes;
 static u32 __ARQMStackPointer[ARQM_STACKENTRIES];
 static ARQM_Info __ARQMInfo[ARQM_STACKENTRIES];
-static u32 __ARQMZeroBuffer[ARQM_ZEROBYTES/sizeof(u32)] ATTRIBUTE_ALIGN(32);
+static u8 __ARQMZeroBuffer[ARQM_ZEROBYTES] ATTRIBUTE_ALIGN(32);
 
-static void __ARQMPollCallback(u32 task)
+static void __ARQMPollCallback(ARQRequest *req)
 {
 	u32 i;
 	ARQM_Info *ptr = NULL;
 	ARQMCallback tccb;
 
 	for(i=0;i<ARQM_STACKENTRIES;i++) {
-		if((ARQRequest*)task==&__ARQMInfo[i].arqhandle) {
-			ptr = &__ARQMInfo[i];
-			break;
-		}
+		ptr = &__ARQMInfo[i];
+		if(req==&ptr->arqhandle) break;
 	}
 	if(i>=ARQM_STACKENTRIES) return;
 
 	tccb = ptr->callback;
 	ptr->callback = NULL;
-	ptr->poll_flag = TRUE;
-	if(tccb) tccb();
+	ptr->polled = TRUE;
 }
 
-void ARQM_Init(u32 arambase,u32 len)
+void ARQM_Init(u32 arambase,s32 len)
 {
 	u32 i;
 	
-	if((s32)len<=0) return;
+	if(len<=0) return;
 
 	__ARQMStackLocation = 0;
 	__ARQMStackPointer[0] = arambase;
 	__ARQMFreeBytes = len;
 	
-	for(i=0;i<ARQM_ZEROBYTES/sizeof(u32);i++) __ARQMZeroBuffer[i] = 0;
+	for(i=0;i<ARQM_ZEROBYTES/sizeof(u32);i++) ((u32*)__ARQMZeroBuffer)[i] = 0;
+	ARQM_PushData(__ARQMZeroBuffer,ARQM_ZEROBYTES);
+	
 }
 
-u32 ARQM_PushData(void *buff,u32 len,ARQMCallback tccb)
+u32 ARQM_PushData(void *buffer,s32 len)
 {
 	u32 rlen,level;
 	ARQM_Info *ptr;
 
-	if(!(((u32)buff)&0x1f) || (s32)len<=0) return -1;
+	if(((u32)buffer)&0x1f || len<=0) return 0;
 
-	rlen = (len+0x1f)&~0x1f;
-	if(__ARQMFreeBytes>=rlen && __ARQMStackLocation<=(ARQM_STACKENTRIES-1)) {
+	rlen = ROUNDUP32(len);
+	if(__ARQMFreeBytes>=rlen && __ARQMStackLocation<(ARQM_STACKENTRIES-1)) {
 		ptr = &__ARQMInfo[__ARQMStackLocation];
 		
 		_CPU_ISR_Disable(level);
-		ptr->aram_start = __ARQMStackPointer[__ARQMStackLocation];
-		__ARQMStackLocation++;
+		ptr->polled = FALSE;
+		ptr->aram_start = __ARQMStackPointer[__ARQMStackLocation++];
 		__ARQMStackPointer[__ARQMStackLocation] = ptr->aram_start+rlen;
 		__ARQMFreeBytes -= rlen;
-		ptr->callback = tccb;
-		ptr->poll_flag = FALSE;
 
-		ARQ_PostRequest(&ptr->arqhandle,__ARQMStackLocation-1,ARQ_MRAMTOARAM,ARQ_PRIO_HI,(u32)buff,ptr->aram_start,rlen,__ARQMPollCallback);
+		ARQ_PostRequestAsync(&ptr->arqhandle,__ARQMStackLocation-1,ARQ_MRAMTOARAM,ARQ_PRIO_HI,ptr->aram_start,(u32)buffer,rlen,__ARQMPollCallback);
 		_CPU_ISR_Restore(level);
 
+		while(ptr->polled==FALSE);
 		return (ptr->aram_start);
 	}
 	return 0;
+}
+
+void ARQM_Pop()
+{
+	u32 level;
+
+	_CPU_ISR_Disable(level);
+
+	if(__ARQMStackLocation>1) {
+		__ARQMFreeBytes += (__ARQMStackPointer[__ARQMStackLocation]-__ARQMStackPointer[__ARQMStackLocation-1]);
+		__ARQMStackLocation--;
+	}
+	_CPU_ISR_Restore(level);
+}
+
+u32 ARQM_GetZeroBuffer()
+{
+	return __ARQMStackPointer[0];
+}
+
+u32 ARQM_GetStackPointer()
+{
+	u32 level,tmp;
+
+	_CPU_ISR_Disable(level)
+	tmp = __ARQMStackPointer[__ARQMStackLocation];
+	_CPU_ISR_Restore(level);
+
+	return tmp;
+}
+
+u32 ARQM_GetFreeSize()
+{
+	u32 level,tmp;
+
+	_CPU_ISR_Disable(level)
+	tmp = __ARQMFreeBytes;
+	_CPU_ISR_Restore(level);
+
+	return tmp;
 }
