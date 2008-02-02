9041dea734d87075b66c0f5dbbc3b8773997fb63
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2008-02-02 15:31:04 +0000

    added for USBGecko support

--- libdb/usb.h
+++ libdb/usb.h
@@ -1,11 +1,12 @@
 #ifndef __USB_H___
 #define __USB_H___
 
-int usb_checkgecko();
-int usb_recvbuffer(void *buffer,int size);
-int usb_sendbuffer(const void *buffer,int size);
-int usb_recvbuffer_safe(void *buffer,int size);
-int usb_sendbuffer_safe(const void *buffer,int size);
+struct dbginterface* usb_init(s32 channel);
+
+int usb_recvbuffer(s32 chn,void *buffer,int size);
+int usb_sendbuffer(s32 chn,const void *buffer,int size);
+int usb_recvbuffer_safe(s32 chn,void *buffer,int size);
+int usb_sendbuffer_safe(s32 chn,const void *buffer,int size);
 void usb_flush();
 
 #endif
