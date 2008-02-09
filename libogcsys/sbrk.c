372df76db88a4355c90d04d918fcc1a6c2fe9982
Dave Murphy <wntrmute@users.sourceforge.net>
2008-02-09 17:10:06 +0000

    remove abort

--- libogcsys/sbrk.c
+++ libogcsys/sbrk.c
@@ -3,12 +3,8 @@
 #include <_syslist.h>
 #include <stdlib.h>
 #include <unistd.h>
-#ifdef REENTRANT_SYSCALLS_PROVIDED
 #include <reent.h>
-#endif
 #include <errno.h>
-#undef errno
-extern int errno;
 
 #include "asm.h"
 #include "processor.h"
@@ -22,42 +18,30 @@ void* _DEFUN(_sbrk_r,(ptr,incr),
 			 struct _reent *ptr _AND
 			 ptrdiff_t incr)
 {
-	u32 level;
-	char *heap_end = 0;
-	char *prev_heap = 0;
-
-	_CPU_ISR_Disable(level);
-	heap_end = (char*)SYS_GetArenaLo();
-	if((heap_end+incr)>(char*)SYS_GetArenaHi()) {
-		_CPU_ISR_Restore(level);
-		abort();
-	}
-
-	prev_heap = heap_end;
-	SYS_SetArenaLo((void*)(heap_end+incr));
-	_CPU_ISR_Restore(level);
-
-	return (void*)prev_heap;	
-}
 #else
 void* _DEFUN(sbrk,(incr),
 			 int incr)
 {
+	struct _reent *ptr = _REENT;
+#endif
 	u32 level;
 	char *heap_end = 0;
 	char *prev_heap = 0;
 
 	_CPU_ISR_Disable(level);
 	heap_end = (char*)SYS_GetArenaLo();
-	if((heap_end+incr)>(char*)SYS_GetArenaHi()) {
-		_CPU_ISR_Restore(level);
-		abort();
-	}
 
-	prev_heap = heap_end;
-	SYS_SetArenaLo((void*)(heap_end+incr));
+	if((heap_end+incr)>(char*)SYS_GetArenaHi()) {
+
+		ptr->_errno = ENOMEM;
+		prev_heap = (char *)-1;
+
+	} else {
+
+		prev_heap = heap_end;
+		SYS_SetArenaLo((void*)(heap_end+incr));
+	}
 	_CPU_ISR_Restore(level);
 
 	return (void*)prev_heap;	
 }
-#endif
