77d818db50cebbd7ca656d65e4b4145c0dcd733a
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2008-02-16 20:57:37 +0000

    added -mrvl

--- gc/ogc/system.h
+++ gc/ogc/system.h
@@ -44,8 +44,8 @@ distribution.
 #include <ogc/lwp_queue.h>
 #include "gx_struct.h"
 
-#define SYS_BASE_CACHED					0x80000000
-#define SYS_BASE_UNCACHED				0xC0000000
+#define SYS_BASE_CACHED					(0x80000000)
+#define SYS_BASE_UNCACHED				(0xC0000000)
 
 #define SYS_WD_NULL						0xffffffff
 
@@ -99,13 +99,13 @@ distribution.
  * @{
  */
 
-#define MEM_VIRTUAL_TO_PHYSICAL(x)		(((u32)(x)) & ~0xC0000000)											/*!< Cast virtual address to physical address, e.g. 0x8xxxxxxx -> 0x0xxxxxxx */
+#define MEM_VIRTUAL_TO_PHYSICAL(x)		(((u32)(x)) & ~SYS_BASE_UNCACHED)									/*!< Cast virtual address to physical address, e.g. 0x8xxxxxxx -> 0x0xxxxxxx */
 #define MEM_PHYSICAL_TO_K0(x)			(void*)((u32)(x) + SYS_BASE_CACHED)									/*!< Cast physical address to cached virtual address, e.g. 0x0xxxxxxx -> 0x8xxxxxxx */
 #define MEM_PHYSICAL_TO_K1(x)			(void*)((u32)(x) + SYS_BASE_UNCACHED)								/*!< Cast physical address to uncached virtual address, e.g. 0x0xxxxxxx -> 0xCxxxxxxx */
 #define MEM_K0_TO_PHYSICAL(x)			(void*)((u32)(x) - SYS_BASE_CACHED)									/*!< Cast physical address to cached virtual address, e.g. 0x0xxxxxxx -> 0x8xxxxxxx */
 #define MEM_K1_TO_PHYSICAL(x)			(void*)((u32)(x) - SYS_BASE_UNCACHED)								/*!< Cast physical address to uncached virtual address, e.g. 0x0xxxxxxx -> 0xCxxxxxxx */
 #define MEM_K0_TO_K1(x)					(void*)((u32)(x) + (SYS_BASE_UNCACHED - SYS_BASE_CACHED))			/*!< Cast cached virtual address to uncached virtual address, e.g. 0x8xxxxxxx -> 0xCxxxxxxx */
-#define MEM_K1_TO_K0(x)					(void*)((u32)(x) - (SYS_BASE_UNCACHED - SYS_BASE_CACHED))										/*!< Cast uncached virtual address to cached virtual address, e.g. 0xCxxxxxxx -> 0x8xxxxxxx */
+#define MEM_K1_TO_K0(x)					(void*)((u32)(x) - (SYS_BASE_UNCACHED - SYS_BASE_CACHED))			/*!< Cast uncached virtual address to cached virtual address, e.g. 0xCxxxxxxx -> 0x8xxxxxxx */
 
 /*!
  *@}
