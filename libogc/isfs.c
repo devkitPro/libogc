b1d15411cde0a13fbcb9dc33511d7e8e6c08b60a
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2008-03-13 15:51:34 +0000

    wrapped RVL define around

--- libogc/isfs.c
+++ libogc/isfs.c
@@ -1,3 +1,35 @@
+/*-------------------------------------------------------------
+
+es.c -- ETicket services
+
+Copyright (C) 2008
+Michael Wiedenbauer (shagkur)
+Dave Murphy (WinterMute)
+Hector Martin (marcan)
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any
+damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any
+purpose, including commercial applications, and to alter it and
+redistribute it freely, subject to the following restrictions:
+
+1.	The origin of this software must not be misrepresented; you
+must not claim that you wrote the original software. If you use
+this software in a product, an acknowledgment in the product
+documentation would be appreciated but is not required.
+
+2.	Altered source versions must be plainly marked as such, and
+must not be misrepresented as being the original software.
+
+3.	This notice may not be removed or altered from any source
+distribution.
+
+-------------------------------------------------------------*/
+
+#if defined(HW_RVL)
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -551,3 +583,5 @@ s32 ISFS_GetFileStatsAsync(s32 fd,fstats *status,isfscallback cb,void *usrdata)
 	param->functype = ISFS_FUNCNULL;
 	return IOS_IoctlAsync(fd,ISFS_IOCTL_GETFILESTATS,NULL,0,status,sizeof(fstats),__isfsFunctionCB,param);
 }
+
+#endif /* defined(HW_RVL) */
