2312b45e9c4fb509a5489a4ceea49173f8a4523b
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2008-03-31 14:31:56 +0000

    fixed c++ issue

--- gc/ogc/usbgecko.h
+++ gc/ogc/usbgecko.h
@@ -3,11 +3,19 @@
 
 #include <gctypes.h>
 
+#ifdef __cplusplus
+   extern "C" {
+#endif /* __cplusplus */
+
 void usb_flush(s32 chn);
 int usb_isgeckoalive(s32 chn);
 int usb_recvbuffer(s32 chn,void *buffer,int size);
 int usb_sendbuffer(s32 chn,const void *buffer,int size);
 int usb_recvbuffer_safe(s32 chn,void *buffer,int size);
 int usb_sendbuffer_safe(s32 chn,const void *buffer,int size);
+
+#ifdef __cplusplus
+   }
+#endif /* __cplusplus */
 
 #endif
