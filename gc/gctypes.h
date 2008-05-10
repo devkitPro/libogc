24f203ebf814967b57e6c7871945dfcbe94becb2
Dave Murphy <wntrmute@users.sourceforge.net>
2008-05-10 17:52:04 +0000

    add argv struct definitition

--- gc/gctypes.h
+++ gc/gctypes.h
@@ -74,6 +74,27 @@ typedef unsigned int BOOL;
 #endif
 /*+----------------------------------------------------------------------------------------------+*/
 
+//!	argv structure
+/*!	\struct __argv
+
+	structure used to set up argc/argv
+
+*/
+struct __argv {
+	int argvMagic;		//!< argv magic number, set to 0x5f617267 ('_arg') if valid 
+	char *commandLine;	//!< base address of command line, set of null terminated strings
+	int length;//!< total length of command line
+	int argc;
+	char **argv;
+	char **endARGV;
+};
+
+//!	Default location for the system argv structure.
+extern struct __argv *__system_argv;
+
+// argv struct magic number
+#define ARGV_MAGIC 0x5f617267
+
 #ifdef __cplusplus
    }
 #endif /* __cplusplus */
