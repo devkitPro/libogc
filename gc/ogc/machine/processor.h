e4f564c1efb821b4aad2011f568eec61c52e1b34
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2008-08-28 21:18:12 +0000

    fixed typo.

--- gc/ogc/machine/processor.h
+++ gc/ogc/machine/processor.h
@@ -106,8 +106,9 @@
 		"ori %1,%0,0x8000\n" \
 		"mtmsr %1\n" \
 		"extrwi %0,%0,1,16" \
-		: "=&r" ((_isr_cookie),"=&r"((_enable_mask)) \
-		: "0" ((_isr_cookie)),"1"((_enable_mask)));\
+		: "=&r" ((_isr_cookie)), "=&r" ((_enable_mask)) \
+		: "0" ((_isr_cookie)), "1" ((_enable_mask)) \
+	  ); \
 	}
 
 #define _CPU_ISR_Disable( _isr_cookie ) \
