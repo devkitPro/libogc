f7ff78085a05f17bb28c95337dc1466e4a596e2a
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2009-01-03 11:04:34 +0000

    added MP3Player_Volume

--- gc/mp3player.h
+++ gc/mp3player.h
@@ -11,6 +11,7 @@
 void MP3Player_Init();
 void MP3Player_Stop();
 BOOL MP3Player_IsPlaying();
+void MP3Player_Volume(u32 volume);
 s32 MP3Player_PlayBuffer(const void *buffer,s32 len,void (*filterfunc)(struct mad_stream *,struct mad_frame *));
 s32 MP3Player_PlayFile(void *cb_data,s32 (*reader)(void *,void *,s32),void (*filterfunc)(struct mad_stream *,struct mad_frame *));
 
