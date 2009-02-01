d9fb680fef86633736afbd9d346c393135e9a9ee
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2009-02-01 11:14:58 +0000

    added gx shadow register definition map

---
+++ libogc/gx_regdef.h
@@ -0,0 +1,72 @@
+#ifndef __GX_REGDEF_H__
+#define __GX_REGDEF_H__
+
+#include <gctypes.h>
+
+#define STRUCT_REGDEF_SIZE		1280
+
+struct _gx_regdef
+{
+	u16 cpSRreg;
+	u16 cpCRreg;
+	u16 cpCLreg;
+	u8 saveDLctx;
+	u8 gxFifoUnlinked;
+	u8 texCopyZTex;
+	u16 xfFlush;
+	u16 xfFlushExp;
+	u16 xfFlushSafe;
+	u32 gxFifoInited;
+	u32 vcdClear;
+	u32 VATTable;
+	u32 mtxIdxLo;
+	u32 mtxIdxHi;
+	u32 texCoordManually;
+	u32 vcdLo;
+	u32 vcdHi;
+	u32 vcdNrms;
+	u32 dirtyState;
+	u32 perf0Mode;
+	u32 perf1Mode;
+	u32 cpPerfMode;
+	u32 VAT0reg[8];
+	u32 VAT1reg[8];
+	u32 VAT2reg[8];
+	u32 texMapSize[8];
+	u32 texMapWrap[8];
+	u32 sciTLcorner;
+	u32 sciBRcorner;
+	u32 lpWidth;
+	u32 genMode;
+	u32 suSsize[8];
+	u32 suTsize[8];
+	u32 tevTexMap[16];
+	u32 tevColorEnv[16];
+	u32 tevAlphaEnv[16];
+	u32 tevSwapModeTable[8];
+	u32 tevRasOrder[11];
+	u32 tevTexCoordEnable;
+	u32 tevIndMask;
+	u32 texCoordGen[8];
+	u32 texCoordGen2[8];
+	u32 dispCopyCntrl;
+	u32 dispCopyDst;
+	u32 dispCopyTL;
+	u32 dispCopyWH;
+	u32 texCopyCntrl;
+	u32 texCopyDst;
+	u32 texCopyTL;
+	u32 texCopyWH;
+	u32 peZMode;
+	u32 peCMode0;
+	u32 peCMode1;
+	u32 peCntrl;
+	u32 chnAmbColor[2];
+	u32 chnMatColor[2];
+	u32 chnCntrl[4];
+	u8 _pad[1];
+	GXTexRegion texRegion[12];
+	GXTlutRegion tlutRegion[20];
+} __attribute__((packed));
+
+#endif
