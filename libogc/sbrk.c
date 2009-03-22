dba27181e6dd91722236cc53a9f8ef687ed13066
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2009-03-22 15:32:18 +0000

    - added MEM2 sbrk patch from marcan. With possibility to turn it off. Declare "u32 MALLOC_MEM2 = 0;" at global scope in your project

--- libogc/sbrk.c
+++ libogc/sbrk.c
@@ -9,6 +9,10 @@
 #include "processor.h"
 #include "system.h"
 
+#if defined(HW_RVL)
+u32 MALLOC_MEM2 __attribute__((weak)) = 1;
+#endif
+
 void* _DEFUN(__libogc_sbrk_r,(ptr,incr),
 			 struct _reent *ptr _AND
 			 ptrdiff_t incr)
@@ -16,20 +20,72 @@ void* _DEFUN(__libogc_sbrk_r,(ptr,incr),
 	u32 level;
 	char *heap_end = 0;
 	char *prev_heap = 0;
+#if defined(HW_RVL)
+	static char *mem2_start = NULL;
+#endif
 
 	_CPU_ISR_Disable(level);
-	heap_end = (char*)SYS_GetArenaLo();
+#if defined(HW_RVL)
+	if(MALLOC_MEM2) {
+		// use MEM2 aswell for malloc
+		if(mem2_start==NULL)
+			heap_end = (char*)SYS_GetArenaLo();
+		else
+			heap_end = (char*)SYS_GetArena2Lo();
 
-	if((heap_end+incr)>(char*)SYS_GetArenaHi()) {
-
-		ptr->_errno = ENOMEM;
-		prev_heap = (char *)-1;
-
+		if(mem2_start) {
+			// we're in MEM2
+			if((heap_end+incr)>(char*)SYS_GetArena2Hi()) {
+				// out of MEM2 case
+				ptr->_errno = ENOMEM;
+				prev_heap = (char *)-1;
+			} else if ((heap_end+incr) < mem2_start) {
+				// trying to sbrk() back below the MEM2 start barrier
+				ptr->_errno = EINVAL;
+				prev_heap = (char *)-1;
+			} else {
+				// success case
+				prev_heap = heap_end;
+				SYS_SetArena2Lo((void*)(heap_end+incr));
+			}
+			// if MEM2 area is exactly at the barrier, transition back to MEM1 again
+			if(SYS_GetArena2Lo() == mem2_start) mem2_start = NULL;
+		} else {
+			// we're in MEM1
+			if((heap_end+incr)>(char*)SYS_GetArenaHi()) {
+				// out of MEM1, transition into MEM2
+				if(((char*)SYS_GetArena2Lo() + incr) > (char*)SYS_GetArena2Hi()) {
+					// this increment doesn't fit in available MEM2
+					ptr->_errno = ENOMEM;
+					prev_heap = (char *)-1;
+				} else {
+					// MEM2 is available, move into it
+					mem2_start = heap_end = prev_heap = SYS_GetArena2Lo();
+					SYS_SetArena2Lo((void*)(heap_end+incr));
+				}
+			} else {
+				// MEM1 is available (or we're freeing memory)
+				prev_heap = heap_end;
+				SYS_SetArenaLo((void*)(heap_end+incr));
+			}
+		}
 	} else {
+#endif
+		heap_end = (char*)SYS_GetArenaLo();
 
-		prev_heap = heap_end;
-		SYS_SetArenaLo((void*)(heap_end+incr));
+		if((heap_end+incr)>(char*)SYS_GetArenaHi()) {
+
+			ptr->_errno = ENOMEM;
+			prev_heap = (char *)-1;
+
+		} else {
+
+			prev_heap = heap_end;
+			SYS_SetArenaLo((void*)(heap_end+incr));
+		}
+#if defined(HW_RVL)
 	}
+#endif
 	_CPU_ISR_Restore(level);
 
 	return (void*)prev_heap;	
