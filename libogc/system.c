34228492c901c65f5454ea2b7085f077926f7347
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2009-05-04 10:41:56 +0000

    - added user callback argument parameter to SYS_Alarm functions.

--- libogc/system.c
+++ libogc/system.c
@@ -110,6 +110,7 @@ typedef struct _alarm_st
 	u64 periodic;
 	u64 start_per;
 	alarmcallback alarmhandler;
+	void *cb_arg;
 } alarm_st;
 
 typedef struct _yay0header {
@@ -307,7 +308,7 @@ static void __sys_alarmhandler(void *arg)
 	__lwp_thread_dispatchdisable();
 	alarm = (alarm_st*)__lwp_objmgr_getnoprotection(&sys_alarm_objects,LWP_OBJMASKID(thealarm));
 	if(alarm) {
-		if(alarm->alarmhandler) alarm->alarmhandler(thealarm);
+		if(alarm->alarmhandler) alarm->alarmhandler(thealarm,alarm->cb_arg);
 		if(alarm->periodic) __lwp_wd_insert_ticks(&alarm->alarm,alarm->periodic);
 	}
 	__lwp_thread_dispatchunnest();
@@ -1523,13 +1524,14 @@ s32 SYS_CreateAlarm(syswd_t *thealarm)
 	return 0;
 }
 
-s32 SYS_SetAlarm(syswd_t thealarm,const struct timespec *tp,alarmcallback cb)
+s32 SYS_SetAlarm(syswd_t thealarm,const struct timespec *tp,alarmcallback cb,void *cbarg)
 {
 	alarm_st *alarm;
 
 	alarm = __lwp_syswd_open(thealarm);
 	if(!alarm) return -1;
 
+	alarm->cb_arg = cbarg;
 	alarm->alarmhandler = cb;
 	alarm->ticks = __lwp_wd_calc_ticks(tp);
 
@@ -1542,7 +1544,7 @@ s32 SYS_SetAlarm(syswd_t thealarm,const struct timespec *tp,alarmcallback cb)
 	return 0;
 }
 
-s32 SYS_SetPeriodicAlarm(syswd_t thealarm,const struct timespec *tp_start,const struct timespec *tp_period,alarmcallback cb)
+s32 SYS_SetPeriodicAlarm(syswd_t thealarm,const struct timespec *tp_start,const struct timespec *tp_period,alarmcallback cb,void *cbarg)
 {
 	alarm_st *alarm;
 
@@ -1552,6 +1554,7 @@ s32 SYS_SetPeriodicAlarm(syswd_t thealarm,const struct timespec *tp_start,const 
 	alarm->start_per = __lwp_wd_calc_ticks(tp_start);
 	alarm->periodic = __lwp_wd_calc_ticks(tp_period);
 	alarm->alarmhandler = cb;
+	alarm->cb_arg = cbarg;
 
 	alarm->ticks = 0;
 
