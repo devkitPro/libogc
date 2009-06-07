778da9df2acca4b549f6f3ab9812822cb14baef0
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2009-06-07 11:44:32 +0000

    - added GX_PeekARGB & GX_PeekZ for direct access of EFB

--- gc/ogc/gx.h
+++ gc/ogc/gx.h
@@ -1717,7 +1717,9 @@ void GX_PokeBlendMode(u8 type,u8 src_fact,u8 dst_fact,u8 op);
 void GX_PokeAlphaRead(u8 mode);
 void GX_PokeDstAlpha(u8 enable,u8 a);
 void GX_PokeARGB(u16 x,u16 y,GXColor color);
+void GX_PeekARGB(u16 x,u16 y,GXColor *color);
 void GX_PokeZ(u16 x,u16 y,u32 z);
+void GX_PeekZ(u16 x,u16 y,u32 *z);
 void GX_PokeZMode(u8 comp_enable,u8 func,u8 update_enable);
 
 u32 GX_GetTexObjFmt(GXTexObj *obj);
