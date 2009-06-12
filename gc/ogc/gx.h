46ea6be17b26787060412848b9d0f2efcce29c45
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2009-06-12 09:20:33 +0000

    - changed from volatile* to volatile members for WGPipe

--- gc/ogc/gx.h
+++ gc/ogc/gx.h
@@ -941,13 +941,13 @@
 
 typedef union _wgpipe
 {
-	u8 U8;
-	s8 S8;
-	u16 U16;
-	s16 S16;
-	u32 U32;
-	s32 S32;
-	f32 F32;
+	vu8 U8;
+	vs8 S8;
+	vu16 U16;
+	vs16 S16;
+	vu32 U32;
+	vs32 S32;
+	vf32 F32;
 } WGPipe;
 
 typedef struct _gx_color {
@@ -1035,7 +1035,7 @@ typedef GXTexRegion* (*GXTexRegionCallback)(GXTexObj *obj,u8 mapid);
 */
 typedef GXTlutRegion* (*GXTlutRegionCallback)(u32 tlut_name);
 
-extern volatile WGPipe* const _wgPipe;
+extern WGPipe* const wgPipe;
 /*! 
  * \fn GXFifoObj* GX_Init(void *base,u32 size)
  * \brief Initializes the graphics processor to its initial state and should be called before any other GX functions.
@@ -1140,221 +1140,221 @@ static inline void GX_End()
 
 static inline void GX_Position3f32(f32 x,f32 y,f32 z)
 {
-	_wgPipe->F32 = x;
-	_wgPipe->F32 = y;
-	_wgPipe->F32 = z;
+	wgPipe->F32 = x;
+	wgPipe->F32 = y;
+	wgPipe->F32 = z;
 }
 
 static inline void GX_Position3u16(u16 x,u16 y,u16 z)
 {
-	_wgPipe->U16 = x;
-	_wgPipe->U16 = y;
-	_wgPipe->U16 = z;
+	wgPipe->U16 = x;
+	wgPipe->U16 = y;
+	wgPipe->U16 = z;
 }
 
 static inline void GX_Position3s16(s16 x,s16 y,s16 z)
 {
-	_wgPipe->S16 = x;
-	_wgPipe->S16 = y;
-	_wgPipe->S16 = z;
+	wgPipe->S16 = x;
+	wgPipe->S16 = y;
+	wgPipe->S16 = z;
 }
 
 static inline void GX_Position3u8(u8 x,u8 y,u8 z)
 {
-	_wgPipe->U8 = x;
-	_wgPipe->U8 = y;
-	_wgPipe->U8 = z;
+	wgPipe->U8 = x;
+	wgPipe->U8 = y;
+	wgPipe->U8 = z;
 }
 
 static inline void GX_Position3s8(s8 x,s8 y,s8 z)
 {
-	_wgPipe->S8 = x;
-	_wgPipe->S8 = y;
-	_wgPipe->S8 = z;
+	wgPipe->S8 = x;
+	wgPipe->S8 = y;
+	wgPipe->S8 = z;
 }
 
 static inline void GX_Position2f32(f32 x,f32 y)
 {
-	_wgPipe->F32 = x;
-	_wgPipe->F32 = y;
+	wgPipe->F32 = x;
+	wgPipe->F32 = y;
 }
 
 static inline void GX_Position2u16(u16 x,u16 y)
 {
-	_wgPipe->U16 = x;
-	_wgPipe->U16 = y;
+	wgPipe->U16 = x;
+	wgPipe->U16 = y;
 }
 
 static inline void GX_Position2s16(s16 x,s16 y)
 {
-	_wgPipe->S16 = x;
-	_wgPipe->S16 = y;
+	wgPipe->S16 = x;
+	wgPipe->S16 = y;
 }
 
 static inline void GX_Position2u8(u8 x,u8 y)
 {
-	_wgPipe->U8 = x;
-	_wgPipe->U8 = y;
+	wgPipe->U8 = x;
+	wgPipe->U8 = y;
 }
 
 static inline void GX_Position2s8(s8 x,s8 y)
 {
-	_wgPipe->S8 = x;
-	_wgPipe->S8 = y;
+	wgPipe->S8 = x;
+	wgPipe->S8 = y;
 }
 
 static inline void GX_Position1x8(u8 index)
 {
-	_wgPipe->U8 = index;
+	wgPipe->U8 = index;
 }
 
 static inline void GX_Position1x16(u16 index)
 {
-	_wgPipe->U16 = index;
+	wgPipe->U16 = index;
 }
 
 static inline void GX_Normal3f32(f32 nx,f32 ny,f32 nz)
 {
-	_wgPipe->F32 = nx;
-	_wgPipe->F32 = ny;
-	_wgPipe->F32 = nz;
+	wgPipe->F32 = nx;
+	wgPipe->F32 = ny;
+	wgPipe->F32 = nz;
 }
 
 static inline void GX_Normal3s16(s16 nx,s16 ny,s16 nz)
 {
-	_wgPipe->S16 = nx;
-	_wgPipe->S16 = ny;
-	_wgPipe->S16 = nz;
+	wgPipe->S16 = nx;
+	wgPipe->S16 = ny;
+	wgPipe->S16 = nz;
 }
 
 static inline void GX_Normal3s8(s8 nx,s8 ny,s8 nz)
 {
-	_wgPipe->S8 = nx;
-	_wgPipe->S8 = ny;
-	_wgPipe->S8 = nz;
+	wgPipe->S8 = nx;
+	wgPipe->S8 = ny;
+	wgPipe->S8 = nz;
 }
 
 static inline void GX_Normal1x8(u8 index)
 {
-	_wgPipe->U8 = index;
+	wgPipe->U8 = index;
 }
 
 static inline void GX_Normal1x16(u16 index)
 {
-	_wgPipe->U16 = index;
+	wgPipe->U16 = index;
 }
 
 static inline void GX_Color4u8(u8 r,u8 g,u8 b,u8 a)
 {
-	_wgPipe->U8 = r;
-	_wgPipe->U8 = g;
-	_wgPipe->U8 = b;
-	_wgPipe->U8 = a;
+	wgPipe->U8 = r;
+	wgPipe->U8 = g;
+	wgPipe->U8 = b;
+	wgPipe->U8 = a;
 }
 
 static inline void GX_Color3u8(u8 r,u8 g,u8 b)
 {
-	_wgPipe->U8 = r;
-	_wgPipe->U8 = g;
-	_wgPipe->U8 = b;
+	wgPipe->U8 = r;
+	wgPipe->U8 = g;
+	wgPipe->U8 = b;
 }
 
 static inline void GX_Color3f32(f32 r, f32 g, f32 b)
 {
-	_wgPipe->U8 = (u8)(r * 255.0);
-	_wgPipe->U8 = (u8)(g * 255.0);
-	_wgPipe->U8 = (u8)(b * 255.0);
+	wgPipe->U8 = (u8)(r * 255.0);
+	wgPipe->U8 = (u8)(g * 255.0);
+	wgPipe->U8 = (u8)(b * 255.0);
 
 }
 
 static inline void GX_Color1u32(u32 clr)
 {
-	_wgPipe->U32 = clr;
+	wgPipe->U32 = clr;
 }
 
 static inline void GX_Color1u16(u16 clr)
 {
-	_wgPipe->U16 = clr;
+	wgPipe->U16 = clr;
 }
 
 static inline void GX_Color1x8(u8 index)
 {
-	_wgPipe->U8 = index;
+	wgPipe->U8 = index;
 }
 
 static inline void GX_Color1x16(u16 index)
 {
-	_wgPipe->U16 = index;
+	wgPipe->U16 = index;
 }
 
 static inline void GX_TexCoord2f32(f32 s,f32 t)
 {
-	_wgPipe->F32 = s;
-	_wgPipe->F32 = t;
+	wgPipe->F32 = s;
+	wgPipe->F32 = t;
 }
 
 static inline void GX_TexCoord2u16(u16 s,u16 t)
 {
-	_wgPipe->U16 = s;
-	_wgPipe->U16 = t;
+	wgPipe->U16 = s;
+	wgPipe->U16 = t;
 }
 
 static inline void GX_TexCoord2s16(s16 s,s16 t)
 {
-	_wgPipe->S16 = s;
-	_wgPipe->S16 = t;
+	wgPipe->S16 = s;
+	wgPipe->S16 = t;
 }
 
 static inline void GX_TexCoord2u8(u8 s,u8 t)
 {
-	_wgPipe->U8 = s;
-	_wgPipe->U8 = t;
+	wgPipe->U8 = s;
+	wgPipe->U8 = t;
 }
 
 static inline void GX_TexCoord2s8(s8 s,s8 t)
 {
-	_wgPipe->S8 = s;
-	_wgPipe->S8 = t;
+	wgPipe->S8 = s;
+	wgPipe->S8 = t;
 }
 
 static inline void GX_TexCoord1f32(f32 s)
 {
-	_wgPipe->F32 = s;
+	wgPipe->F32 = s;
 }
 
 static inline void GX_TexCoord1u16(u16 s)
 {
-	_wgPipe->U16 = s;
+	wgPipe->U16 = s;
 }
 
 static inline void GX_TexCoord1s16(s16 s)
 {
-	_wgPipe->S16 = s;
+	wgPipe->S16 = s;
 }
 
 static inline void GX_TexCoord1u8(u8 s)
 {
-	_wgPipe->U8 = s;
+	wgPipe->U8 = s;
 }
 
 static inline void GX_TexCoord1s8(s8 s)
 {
-	_wgPipe->S8 = s;
+	wgPipe->S8 = s;
 }
 
 static inline void GX_TexCoord1x8(u8 index)
 {
-	_wgPipe->U8 = index;
+	wgPipe->U8 = index;
 }
 
 static inline void GX_TexCoord1x16(u16 index)
 {
-	_wgPipe->U16 = index;
+	wgPipe->U16 = index;
 }
 
 static inline void GX_MatrixIndex1x8(u8 index)
 {
-	_wgPipe->U8 = index;
+	wgPipe->U8 = index;
 }
 
 void GX_AdjustForOverscan(GXRModeObj *rmin,GXRModeObj *rmout,u16 hor,u16 ver);
