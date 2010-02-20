5d6bbe037846105707020a3b62e0bb504bf039c2
Dave Murphy <wntrmute@users.sourceforge.net>
2010-02-20 10:26:53 +0000

    use stdint.h for cardinal types

--- gc/gctypes.h
+++ gc/gctypes.h
@@ -7,37 +7,38 @@
 */ 
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
    extern "C" {
 #endif /* __cplusplus */
 
 /*+----------------------------------------------------------------------------------------------+*/
-typedef unsigned char u8;									///< 8bit unsigned integer
-typedef unsigned short u16;								///< 16bit unsigned integer
-typedef unsigned int u32;									///< 32bit unsigned integer
-typedef unsigned long long u64;						///< 64bit unsigned integer
+typedef uint8_t u8;					///< 8bit unsigned integer
+typedef uint16_t u16;				///< 16bit unsigned integer
+typedef uint32_t u32;				///< 32bit unsigned integer
+typedef uint64_t u64;				///< 64bit unsigned integer
 /*+----------------------------------------------------------------------------------------------+*/
-typedef signed char s8;										///< 8bit signed integer
-typedef signed short s16;									///< 16bit signed integer
-typedef signed int s32;										///< 32bit signed integer
-typedef signed long long s64;							///< 64bit signed integer
+typedef int8_t s8;					///< 8bit signed integer
+typedef int16_t s16;				///< 16bit signed integer
+typedef int32_t s32;				///< 32bit signed integer
+typedef int64_t s64;				///< 64bit signed integer
 /*+----------------------------------------------------------------------------------------------+*/
-typedef volatile unsigned char vu8;				///< 8bit unsigned volatile integer
-typedef volatile unsigned short vu16;			///< 16bit unsigned volatile integer
-typedef volatile unsigned int vu32;				///< 32bit unsigned volatile integer
-typedef volatile unsigned long long vu64;	///< 64bit unsigned volatile integer
+typedef volatile u8 vu8;			///< 8bit unsigned volatile integer
+typedef volatile u16 vu16;			///< 16bit unsigned volatile integer
+typedef volatile u32 vu32;			///< 32bit unsigned volatile integer
+typedef volatile u64 vu64;			///< 64bit unsigned volatile integer
 /*+----------------------------------------------------------------------------------------------+*/
-typedef volatile signed char vs8;					///< 8bit signed volatile integer
-typedef volatile signed short vs16;				///< 16bit signed volatile integer
-typedef volatile signed int vs32;					///< 32bit signed volatile integer
-typedef volatile signed long long vs64;		///< 64bit signed volatile integer
+typedef volatile s8 vs8;			///< 8bit signed volatile integer
+typedef volatile s16 vs16;			///< 16bit signed volatile integer
+typedef volatile s32 vs32;			///< 32bit signed volatile integer
+typedef volatile s64 vs64;			///< 64bit signed volatile integer
 /*+----------------------------------------------------------------------------------------------+*/
 // fixed point math typedefs
-typedef s16 sfp16;                              ///< signed 8:8 fixed point
-typedef s32 sfp32;                              ///< signed 20:8 fixed point
-typedef u16 ufp16;                              ///< unsigned 8:8 fixed point
-typedef u32 ufp32;                              ///< unsigned 24:8 fixed point
+typedef s16 sfp16;					///< signed 8:8 fixed point
+typedef s32 sfp32;					///< signed 20:8 fixed point
+typedef u16 ufp16;					///< unsigned 8:8 fixed point
+typedef u32 ufp32;					///< unsigned 24:8 fixed point
 /*+----------------------------------------------------------------------------------------------+*/
 typedef float f32;
 typedef double f64;
@@ -50,7 +51,7 @@ typedef volatile double vf64;
 typedef unsigned int BOOL;
 /*+----------------------------------------------------------------------------------------------+*/
 // alias type typedefs
-#define FIXED s32                               ///< Alias type for sfp32
+#define FIXED s32					///< Alias type for sfp32
 /*+----------------------------------------------------------------------------------------------+*/
 // boolean defines
 #ifndef boolean
@@ -58,15 +59,15 @@ typedef unsigned int BOOL;
 #endif
 /*+----------------------------------------------------------------------------------------------+*/
 #ifndef TRUE
-#define TRUE 1                                  ///< True
+#define TRUE	1					///< True
 #endif
 /*+----------------------------------------------------------------------------------------------+*/
 #ifndef FALSE
-#define FALSE 0                                 ///< False
+#define FALSE	0					///< False
 #endif
 /*+----------------------------------------------------------------------------------------------+*/
 #ifndef NULL
-#define NULL			0                        ///< Pointer to 0
+#define NULL	0					///< Pointer to 0
 #endif
 /*+----------------------------------------------------------------------------------------------+*/
 #ifndef LITTLE_ENDIAN
