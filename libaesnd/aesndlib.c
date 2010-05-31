dc58d3bd8303fecb4f93973f2d4df1c7036f2312
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2010-05-31 09:28:05 +0000

    - added improved version of libasnd - called libaesnd. It supports 32 voices now and uses ACCL to read in the samples

---
+++ libaesnd/aesndlib.c
@@ -0,0 +1,624 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <ogcsys.h>
+#include <gccore.h>
+#include <ogc/lwp_watchdog.h>
+#include <ogc/machine/processor.h>
+
+#include "aesndlib.h"
+#include "dspmixer.h"
+
+#define PB_STRUCT_SIZE			64
+#define DSP_DRAMSIZE			8192
+
+#define VOICE_PAUSE				0x00000004
+#define VOICE_LOOP				0x00000008
+#define VOICE_ONCE				0x00000010
+#define VOICE_STREAM			0x00000020
+
+#define VOICE_REQCALLBACK		0x00080000
+#define VOICE_FINISHED			0x00100000
+#define VOICE_STOPPED			0x00200000
+#define VOICE_USED				0x80000000
+
+struct aesndpb_t
+{
+	u32 out_buf;				//0
+
+	u32 buf_start;				//4
+	u32 buf_end;				//8
+	u32 buf_curr;				//12
+
+	u16 yn1;					//16
+	u16 yn2;					//18
+	u16 pds;					//20
+
+	u16 freq_h;					//22
+	u16 freq_l;					//24
+	u16 counter;				//26
+
+	s16 left,right;				//28,30
+	u16 volume_l,volume_r;		//32,34
+
+	u32 delay;					//36
+
+	u32 flags;					//40
+
+	u8 _pad[20];
+
+#if defined(HW_DOL)
+	u32 mram_start;
+	u32 mram_curr;
+	u32 mram_end;
+	u32 aram_last;
+#endif
+
+	u32 voiceno;
+	u32 shift;
+	AESNDVoiceCallback cb;
+	
+	AESNDAudioCallback audioCB;
+} ATTRIBUTE_PACKED;
+
+static dsptask_t __aesnddsptask;
+
+static vu32 __aesndinit = 0;
+static vu32 __aesndcurrab = 0;
+static vu32 __aesnddspinit = 0;
+static vu32 __aesndcurrvoice = 0;
+static vu32 __aesnddspcomplete = 0;
+static vu64 __aesnddspstarttime = 0;
+static vu64 __aesnddspprocesstime = 0;
+static vu32 __aesnddspabrequested = 0;
+static volatile bool __aesndglobalpause = false;
+static volatile bool __aesndvoicesstopped = true;
+
+#if defined(HW_DOL)
+static u32 __aesndarambase = 0;
+static u32 __aesndaramblocks[MAX_VOICES];
+static u32 __aesndarammemory[MAX_VOICES];
+static u8 aram_buffer[SND_BUFFERSIZE*2] ATTRIBUTE_ALIGN(32);
+#endif
+
+static AESNDPB __aesndvoicepb[MAX_VOICES];
+static AESNDPB __aesndcommand ATTRIBUTE_ALIGN(32);
+static u8 __dspdram[DSP_DRAMSIZE] ATTRIBUTE_ALIGN(32);
+static u8 mute_buffer[SND_BUFFERSIZE] ATTRIBUTE_ALIGN(32);
+static u8 audio_buffer[2][SND_BUFFERSIZE] ATTRIBUTE_ALIGN(32);
+
+static __inline__ void snd_set0b(char *p,int n)
+{
+	while(n>0) { *p++ = 0; n--; }
+}
+
+static __inline__ void snd_set0w(int *p,int n)
+{
+	while(n>0) { *p++ = 0; n--; }
+}
+
+static __inline__ void __aesndcopycommand(AESNDPB *dst,AESNDPB *src)
+{
+	dst->buf_start = src->buf_start;
+	dst->buf_end = src->buf_end;
+	dst->buf_curr = src->buf_curr;
+
+	dst->yn1 = src->yn1;
+	dst->yn2 = src->yn2;
+	dst->pds = src->pds;
+
+	dst->freq_h = src->freq_h;
+	dst->freq_l = src->freq_l;
+	dst->counter = src->counter;
+
+	dst->left = src->left;
+	dst->right = src->right;
+
+	dst->volume_l = src->volume_l;
+	dst->volume_r = src->volume_r;
+
+	dst->flags = src->flags;
+	dst->delay = src->delay;
+
+#if defined(HW_DOL)
+	dst->mram_start = src->mram_start;
+	dst->mram_curr = src->mram_curr;
+	dst->mram_end = src->mram_end;
+	dst->aram_last = src->aram_last;
+#endif
+
+	dst->voiceno = src->voiceno;
+	dst->shift = src->shift;
+	dst->cb = src->cb;
+}
+
+static __inline__ void __aesndsetvoiceformat(AESNDPB *pb,u32 format)
+{
+	pb->flags = (pb->flags&~0x03)|(format&0x03);
+	switch((format&0x03)) {
+		case VOICE_MONO8:
+		case VOICE_STEREO8:
+			pb->shift = 0;
+			break;
+		case VOICE_MONO16:
+		case VOICE_STEREO16:
+			pb->shift = 1;
+			break;
+	}
+}
+
+static __inline__ void __aesndsetvoicebuffer(AESNDPB *pb,void* buffer,u32 len)
+{
+#if defined(HW_RVL)
+	register u32 buf_addr;
+	buf_addr = (u32)MEM_VIRTUAL_TO_PHYSICAL(buffer);
+	pb->buf_start = buf_addr>>pb->shift;
+	pb->buf_end = (buf_addr + len - (1<<pb->shift))>>pb->shift;
+	pb->buf_curr = pb->buf_start;
+#elif defined(HW_DOL)
+	pb->mram_start = (u32)buffer;
+	pb->mram_curr = (u32)buffer;
+	pb->mram_end = (u32)buffer + len;
+#endif
+}
+
+static __inline__ void __aesndsetvoicefreq(AESNDPB *pb,u32 freq)
+{
+	register u32 ratio = 0x00010000*((f32)freq/(f32)DSP_DEFAULT_FREQ);
+	pb->freq_h = (u16)(ratio>>16);
+	pb->freq_l = (u16)(ratio&0xffff);
+}
+
+#if defined(HW_DOL)
+static __inline__ void __aesndhandlerequest(AESNDPB *pb)
+{
+	register u32 buf_addr;
+	register u32 copy_len;
+	register u32 rem_len;
+	register u32 buf_size;
+
+	if(pb->mram_curr>=pb->mram_end) {
+		if(pb->flags&VOICE_ONCE) {
+			pb->flags |= VOICE_STOPPED;
+			return;
+		} else if(pb->flags&VOICE_LOOP) pb->mram_curr = pb->mram_start;
+		else if(pb->cb) pb->cb(pb,VOICE_STATE_STREAM);
+	}
+
+	buf_addr = __aesndaramblocks[pb->voiceno];
+	if(pb->buf_start) {
+		u32 curr_pos = pb->buf_curr;
+		if(curr_pos>=pb->buf_start &&
+		   curr_pos<(pb->buf_start + (SND_BUFFERSIZE>>pb->shift)) &&
+		   pb->flags&VOICE_REQCALLBACK) buf_addr += SND_BUFFERSIZE;
+		else if(curr_pos<(pb->buf_start + (SND_BUFFERSIZE>>pb->shift))) return;
+		else if(curr_pos>pb->aram_last && pb->aram_last>=(pb->buf_start + (SND_BUFFERSIZE>>pb->shift))) return;
+
+		pb->aram_last = curr_pos;
+		buf_size = SND_BUFFERSIZE;
+	} else {
+		pb->buf_start = buf_addr>>pb->shift;
+		pb->buf_end = (buf_addr + (SND_BUFFERSIZE*2) - (1<<pb->shift))>>pb->shift;
+		pb->buf_curr = pb->buf_start;
+		buf_size = (SND_BUFFERSIZE*2);
+	}
+
+	rem_len = (pb->mram_end - pb->mram_curr);
+	copy_len = (rem_len<buf_size) ? rem_len : buf_size;
+
+	memcpy(aram_buffer,(void*)pb->mram_curr,copy_len);
+	if(copy_len<buf_size) memset(aram_buffer + copy_len,0,buf_size - copy_len);
+
+	DCFlushRange(aram_buffer,buf_size);
+
+	AR_StartDMA(AR_MRAMTOARAM,(u32)MEM_VIRTUAL_TO_PHYSICAL(aram_buffer),buf_addr,buf_size);
+	while(AR_GetDMAStatus());
+
+	pb->mram_curr += copy_len;
+	pb->flags &= ~VOICE_REQCALLBACK;
+}
+#elif defined(HW_RVL)
+static __inline__ void __aesndhandlerequest(AESNDPB *pb)
+{
+	if(!(pb->flags&VOICE_LOOP) && !(pb->flags&VOICE_ONCE) && pb->cb) pb->cb(pb,VOICE_STATE_STREAM);
+	else if(pb->flags&VOICE_ONCE) pb->flags |= VOICE_STOPPED;
+
+	pb->flags &= ~VOICE_REQCALLBACK;
+}
+#endif
+
+static void __dsp_initcallback(dsptask_t *task)
+{
+	DSP_SendMailTo(0xface0080);
+	while(DSP_CheckMailTo());
+
+	DSP_SendMailTo(MEM_VIRTUAL_TO_PHYSICAL(&__aesndcommand));
+	while(DSP_CheckMailTo());
+
+	__aesnddspinit = 1;
+	__aesnddspcomplete = 1;
+}
+
+static void __dsp_resumecallback(dsptask_t *task)
+{
+}
+
+static void __dsp_requestcallback(dsptask_t *task)
+{
+	if(__aesnddspabrequested==1) {
+		__aesnddspprocesstime = (gettime() - __aesnddspstarttime);
+		__aesnddspabrequested = 0;
+		__aesnddspcomplete = 1;
+		return;
+	}
+
+	DCInvalidateRange(&__aesndcommand,PB_STRUCT_SIZE);
+
+	if(__aesndcommand.flags&VOICE_REQCALLBACK) {
+#if defined(HW_RVL)
+		__aesndhandlerequest(&__aesndcommand);
+#endif
+	}
+
+	if(__aesndcommand.flags&VOICE_FINISHED) {
+		__aesndcommand.flags &= ~VOICE_FINISHED;
+#if defined(HW_DOL)
+		__aesndhandlerequest(&__aesndcommand);
+#endif
+		if(__aesndcommand.flags&VOICE_STOPPED && __aesndcommand.cb) __aesndcommand.cb(&__aesndcommand,VOICE_STATE_STOPPED);
+
+		__aesndcopycommand(&__aesndvoicepb[__aesndcurrvoice],&__aesndcommand);
+
+		__aesndcurrvoice++;
+		while(__aesndcurrvoice<MAX_VOICES && (!(__aesndvoicepb[__aesndcurrvoice].flags&VOICE_USED) || (__aesndvoicepb[__aesndcurrvoice].flags&VOICE_STOPPED))) __aesndcurrvoice++;
+		if(__aesndcurrvoice<MAX_VOICES) {
+			__aesndcopycommand(&__aesndcommand,&__aesndvoicepb[__aesndcurrvoice]);
+			
+			if(__aesndcommand.cb) __aesndcommand.cb(&__aesndcommand,VOICE_STATE_RUNNING);
+
+			DCFlushRange(&__aesndcommand,PB_STRUCT_SIZE);
+			DSP_SendMailTo(0xface0020);
+			while(DSP_CheckMailTo());
+			return;
+		}
+	}
+
+	if(__aesndcurrvoice>=MAX_VOICES) {
+		DSP_SendMailTo(0xface0100);
+		while(DSP_CheckMailTo());
+
+		__aesnddspabrequested = 1;
+	}
+}
+
+static void __dsp_donecallback(dsptask_t *task)
+{
+	__aesnddspinit = 0;
+	__aesnddspcomplete = 0;
+	__aesnddspabrequested = 0;
+}
+
+static void __audio_dma_callback()
+{
+	void *ptr;
+
+	__aesndcurrab ^= 1;
+	if(__aesndglobalpause==true || __aesndvoicesstopped==true)
+		ptr = mute_buffer;
+	else
+		ptr = audio_buffer[__aesndcurrab];
+
+	if(__aesndcommand.audioCB) __aesndcommand.audioCB(ptr,SND_BUFFERSIZE);
+	AUDIO_InitDMA((u32)ptr,SND_BUFFERSIZE);
+
+	if(__aesndglobalpause==true) return;
+	if(!__aesnddspcomplete || !__aesnddspinit) return;
+
+	__aesndcurrvoice = 0;
+	__aesnddspprocesstime = 0;
+	while(__aesndcurrvoice<MAX_VOICES && (!(__aesndvoicepb[__aesndcurrvoice].flags&VOICE_USED) || (__aesndvoicepb[__aesndcurrvoice].flags&VOICE_STOPPED))) __aesndcurrvoice++;
+	if(__aesndcurrvoice>=MAX_VOICES) {
+		__aesndvoicesstopped = true;
+		return;
+	}
+
+	__aesnddspcomplete = 0;
+	__aesndvoicesstopped = false;
+	__aesndcopycommand(&__aesndcommand,&__aesndvoicepb[__aesndcurrvoice]);
+
+	if(__aesndcommand.cb) __aesndcommand.cb(&__aesndcommand,VOICE_STATE_RUNNING);
+
+	__aesndcommand.out_buf = (u32)MEM_VIRTUAL_TO_PHYSICAL(audio_buffer[__aesndcurrab]);
+	DCFlushRange(&__aesndcommand,PB_STRUCT_SIZE);
+
+	__aesnddspstarttime = gettime();
+	DSP_SendMailTo(0xface0010);
+	while(DSP_CheckMailTo());
+}
+
+
+static void __aesndloaddsptask(dsptask_t *task,const void *dsp_code,u32 dsp_code_size,const void *dram_image,u32 dram_size)
+{
+	task->prio = 255;
+	task->iram_maddr = (void*)MEM_VIRTUAL_TO_PHYSICAL(dsp_code);
+	task->iram_len = dsp_code_size;
+	task->iram_addr = 0x0000;
+	task->dram_maddr = (void*)MEM_VIRTUAL_TO_PHYSICAL(dram_image);
+	task->dram_len = dram_size;
+	task->dram_addr = 0x0000;
+	task->init_vec = 0x0010;
+	task->res_cb = __dsp_resumecallback;
+	task->req_cb = __dsp_requestcallback;
+	task->init_cb = __dsp_initcallback;
+	task->done_cb = __dsp_donecallback;
+	DSP_AddTask(task);
+}
+
+void AESND_Init()
+{
+	u32 i,level;
+
+#if defined(HW_DOL)
+	__aesndarambase = AR_Init(__aesndarammemory,MAX_VOICES);
+#endif
+	DSP_Init();
+	AUDIO_Init(NULL);
+	AUDIO_StopDMA();
+	AUDIO_SetDSPSampleRate(AI_SAMPLERATE_48KHZ);
+
+	_CPU_ISR_Disable(level);
+	if(!__aesndinit) {
+		__aesndinit = 1;
+		__aesndcurrab = 0;
+		__aesnddspinit = 0;
+		__aesnddspcomplete = 0;
+		__aesndglobalpause = false;
+		__aesndvoicesstopped = true;
+
+#if defined(HW_DOL)
+		snd_set0w((int*)aram_buffer,SND_BUFFERSIZE>>1);
+		for(i=0;i<MAX_VOICES;i++) __aesndaramblocks[i] = AR_Alloc(SND_BUFFERSIZE*2);
+#endif
+		snd_set0w((int*)mute_buffer,SND_BUFFERSIZE>>2);
+		snd_set0w((int*)audio_buffer[0],SND_BUFFERSIZE>>2);
+		snd_set0w((int*)audio_buffer[1],SND_BUFFERSIZE>>2);
+		DCFlushRange(mute_buffer,SND_BUFFERSIZE);
+		DCFlushRange(audio_buffer[0],SND_BUFFERSIZE);
+		DCFlushRange(audio_buffer[1],SND_BUFFERSIZE);
+
+		snd_set0w((int*)&__aesndcommand,sizeof(struct aesndpb_t)>>2);
+		for(i=0;i<MAX_VOICES;i++)
+			snd_set0w((int*)&__aesndvoicepb[i],sizeof(struct aesndpb_t)>>2);
+
+		__aesndloaddsptask(&__aesnddsptask,dspmixer,dspmixer_size,__dspdram,DSP_DRAMSIZE);
+	}
+
+	AUDIO_RegisterDMACallback(__audio_dma_callback);
+	AUDIO_InitDMA((u32)audio_buffer[__aesndcurrab],SND_BUFFERSIZE);
+	AUDIO_StartDMA();
+
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_Reset()
+{
+	u32 level;
+
+	_CPU_ISR_Disable(level);
+	if(__aesndinit) {
+		AUDIO_StopDMA();
+		AUDIO_RegisterDMACallback(NULL);
+
+		DSP_SendMailTo(0xfacedead);
+		while(DSP_CheckMailTo());
+		
+		do {
+			_CPU_ISR_Flash(level);
+		} while(__aesnddspinit);
+
+		__aesndinit = 0;
+	}
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_Pause(bool pause)
+{
+	u32 level;
+
+	_CPU_ISR_Disable(level);
+	__aesndglobalpause = pause;
+	_CPU_ISR_Restore(level);
+}
+
+u32 AESND_GetDSPProcessTime()
+{
+	u32 level;
+	u32 time = 0;
+
+	_CPU_ISR_Disable(level);
+	time = ticks_to_microsecs(__aesnddspprocesstime);
+	_CPU_ISR_Restore(level);
+	
+	return time;
+}
+
+AESNDAudioCallback AESND_RegisterAudioCallback(AESNDAudioCallback cb)
+{
+	u32 level;
+	AESNDAudioCallback aCB;
+
+	_CPU_ISR_Disable(level);
+	aCB = __aesndcommand.audioCB;
+	__aesndcommand.audioCB = cb;
+	_CPU_ISR_Restore(level);
+
+	return aCB;
+}
+
+AESNDPB* AESND_AllocateVoice(AESNDVoiceCallback cb)
+{
+	u32 i,level;
+	AESNDPB *pb = NULL;
+
+	_CPU_ISR_Disable(level);
+	for(i=0;i<MAX_VOICES;i++) {
+		pb = &__aesndvoicepb[i];
+		if(!(pb->flags&VOICE_USED)) {
+			pb->voiceno = i;
+			pb->flags = (VOICE_USED|VOICE_STOPPED);
+			pb->pds = pb->yn1 = pb->yn2 = 0;
+			pb->buf_start = 0;
+			pb->buf_curr = 0;
+			pb->buf_end = 0;
+			pb->counter = 0;
+			pb->volume_l = 255;
+			pb->volume_r = 255;
+			pb->freq_h = 0x0001;
+			pb->freq_l = 0x0000;
+			pb->cb = cb;
+			break;
+		}
+	}
+	_CPU_ISR_Restore(level);
+
+	return pb;
+}
+
+void AESND_FreeVoice(AESNDPB *pb)
+{
+	u32 level;
+
+	_CPU_ISR_Disable(level);
+	snd_set0w((int*)pb,sizeof(struct aesndpb_t)>>2);
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_PlayVoice(AESNDPB *pb,u32 format,const void *buffer,u32 len,u32 freq,u32 delay,bool looped)
+{
+	u32 level;
+	void *ptr = (void*)buffer;
+
+	_CPU_ISR_Disable(level);
+#if defined(HW_RVL)
+	DCFlushRange(ptr,len);
+#endif
+	__aesndsetvoiceformat(pb,format);
+	__aesndsetvoicefreq(pb,freq);
+	__aesndsetvoicebuffer(pb,ptr,len);
+
+	pb->flags &= ~(VOICE_STOPPED|VOICE_LOOP|VOICE_ONCE);
+	if(looped==true) 
+		pb->flags |= VOICE_LOOP;
+	else
+		pb->flags |= VOICE_ONCE;
+
+#if defined(HW_DOL)
+	pb->buf_start = pb->buf_curr = pb->buf_end = pb->aram_last = 0;
+#endif
+	pb->delay = (delay*48);
+	pb->pds = pb->yn1 = pb->yn2 = 0;
+	pb->counter = 0;
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_SetVoiceBuffer(AESNDPB *pb,const void *buffer,u32 len)
+{
+	u32 level;
+	void *ptr = (void*)buffer;
+
+	_CPU_ISR_Disable(level);
+#if defined(HW_RVL)
+	DCFlushRange(ptr,len);
+#endif
+	__aesndsetvoicebuffer(pb,ptr,len);
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_SetVoiceFormat(AESNDPB *pb,u32 format)
+{
+	u32 level;
+
+	_CPU_ISR_Disable(level);
+	__aesndsetvoiceformat(pb,format);
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_SetVoiceVolume(AESNDPB *pb,u16 volume_l,u16 volume_r)
+{
+	u32 level;
+
+	_CPU_ISR_Disable(level);
+	pb->volume_l = volume_l;
+	pb->volume_r = volume_r;
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_SetVoiceFrequency(AESNDPB *pb,u32 freq)
+{
+	u32 level;
+	
+	_CPU_ISR_Disable(level);
+	__aesndsetvoicefreq(pb,freq);
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_SetVoiceStream(AESNDPB *pb,bool stream)
+{
+	u32 level;
+	
+	_CPU_ISR_Disable(level);
+	if(stream==true)
+		pb->flags |= VOICE_STREAM;
+	else
+		pb->flags &= ~VOICE_STREAM;
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_SetVoiceLoop(AESNDPB *pb,bool loop)
+{
+	u32 level;
+	
+	_CPU_ISR_Disable(level);
+	if(loop==true)
+		pb->flags |= VOICE_LOOP;
+	else
+		pb->flags &= ~VOICE_LOOP;
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_SetVoiceMute(AESNDPB *pb,bool mute)
+{
+	u32 level;
+
+	_CPU_ISR_Disable(level);
+	if(mute==true)
+		pb->flags |= VOICE_PAUSE;
+	else
+		pb->flags &= ~VOICE_PAUSE;
+	_CPU_ISR_Restore(level);
+}
+
+void AESND_SetVoiceStop(AESNDPB *pb,bool stop)
+{
+	u32 level;
+
+	_CPU_ISR_Disable(level);
+	if(stop==true)
+		pb->flags |= VOICE_STOPPED;
+	else
+		pb->flags &= ~VOICE_STOPPED;
+	_CPU_ISR_Restore(level);
+}
+
+AESNDVoiceCallback AESND_RegisterVoiceCallback(AESNDPB *pb,AESNDVoiceCallback cb)
+{
+	u32 level;
+	AESNDVoiceCallback rcb = NULL;
+
+	_CPU_ISR_Disable(level);
+	rcb = pb->cb;
+	pb->cb = cb;
+	_CPU_ISR_Restore(level);
+
+	return rcb;
+}
---
+++ libaesnd/dspcode/dspmixer.s
@@ -0,0 +1,589 @@
+DMACR:			equ		0xffc9
+DMABLEN:		equ		0xffcb
+DMADSPM:		equ		0xffcd
+DMAMMEMH:		equ		0xffce
+DMAMMEML:		equ		0xffcf
+
+ACCOEF:			equ		0xffa0
+
+ACFMT:			equ		0xffd1
+ACSAH:			equ		0xffd4
+ACSAL:			equ		0xffd5
+ACEAH:			equ		0xffd6
+ACEAL:			equ		0xffd7
+ACCAH:			equ		0xffd8
+ACCAL:			equ		0xffd9
+ACPDS:			equ		0xffda
+ACYN1:			equ		0xffdb
+ACYN2:			equ		0xffdc
+ACDAT:			equ		0xffdd
+ACGAN:			equ		0xffde
+
+DIRQ:			equ		0xfffb
+DMBH:			equ		0xfffc
+DMBL:			equ		0xfffd
+CMBH:			equ		0xfffe
+CMBL:			equ		0xffff
+
+DMADMEM:		equ		0x0
+DMAIMEM:		equ		0x2
+DMA_TO_DSP:		equ		0x0
+DMA_TO_CPU:		equ		0x1
+
+/* internal work memory layout */
+MEM_REG:		equ		0x0
+PB_MADDRH:		equ		MEM_REG
+PB_MADDRL:		equ		MEM_REG+0x01
+
+PB_ADDR:		equ		MEM_REG+0x200
+
+/* parameter block layout */
+OUTBUF_ADDRH:	equ		PB_ADDR
+OUTBUF_ADDRL:	equ		PB_ADDR+0x01
+
+SNDBUF_SADDRH:	equ		PB_ADDR+0x02
+SNDBUF_SADDRL:	equ		PB_ADDR+0x03
+SNDBUF_EADDRH:	equ		PB_ADDR+0x04
+SNDBUF_EADDRL:	equ		PB_ADDR+0x05
+SNDBUF_CADDRH:	equ		PB_ADDR+0x06
+SNDBUF_CADDRL:	equ		PB_ADDR+0x07
+
+YN1:			equ		PB_ADDR+0x08
+YN2:			equ		PB_ADDR+0x09
+PDS:			equ		PB_ADDR+0x0a
+
+FREQ_SMPH:		equ		PB_ADDR+0x0b
+FREQ_SMPL:		equ		PB_ADDR+0x0c
+
+SMP_CNT:		equ		PB_ADDR+0x0d
+
+SMP_L:			equ		PB_ADDR+0x0e
+SMP_R:			equ		PB_ADDR+0x0f
+
+VOLUME_L:		equ		PB_ADDR+0x10
+VOLUME_R:		equ		PB_ADDR+0x11
+
+DELAY_SMPH:		equ		PB_ADDR+0x12
+DELAY_SMPL:		equ		PB_ADDR+0x13
+
+FLAGS_SMPH:		equ		PB_ADDR+0x14
+FLAGS_SMPL:		equ		PB_ADDR+0x15
+
+LOOP_PDS:		equ		PB_ADDR+0x16
+LOOP_YN1:		equ		PB_ADDR+0x17
+LOOP_YN2:		equ		PB_ADDR+0x18
+
+/* flags and buffers used */
+MEM_SMP_BUF:		equ		0x0800
+MEM_TMP_BUF:		equ		0x0a00
+MEM_SND_BUF:		equ		0x0c00					//buffer for output sound data, will be DMA'd out to OUTBUF_SND
+
+PB_STURCT_SIZE:		equ		64
+NUM_SAMPLES:		equ		96						//process 2ms of sample data
+DEF_FREQ_INT:		equ		0x0001
+
+VOICE_FLAGL_PAUSE:	equ		0x0004
+VOICE_FLAGL_LOOP:	equ		0x0008
+VOICE_FLAGL_ONCE:	equ		0x0010
+
+VOICE_FLAGH_CB:		equ		0x0008
+VOICE_FLAGH_END:	equ		0x0010
+VOICE_FLAGH_STOP:	equ		0x0020
+
+ACCL_FMT_8BIT:		equ		0x0019
+ACCL_FMT_16BIT:		equ		0x000a
+
+ACCL_GAIN_8BIT:		equ		0x0100
+ACCL_GAIN_16BIT:	equ		0x0800
+
+_start:
+	nop
+	nop
+	jmp		exception1
+	jmp		exception2
+	jmp		exception3
+	jmp		exception4
+	jmp		exception5
+	jmp		exception6
+	jmp		exception7
+	
+	sbset	#0x02
+	sbset	#0x03
+	sbclr	#0x04
+	sbset	#0x05
+	sbset	#0x06
+
+	s16
+	clr15
+	m0
+	lri		$config,#0xff
+	
+	lri		$wr0,#0xffff
+	lri		$wr1,#0xffff
+	lri		$wr2,#0xffff
+	lri		$wr3,#0xffff
+	
+main:
+	si		@DMBH,#0xdcd1
+	si		@DMBL,#0x0000
+	si		@DIRQ,#0x01
+
+recv_cmd:	
+	clr		$acc0
+	clr		$acc1
+	
+	call	wait_mail_sent
+	call	wait_mail_recv
+	
+	lri		$acc0.m,#0xcdd1
+	cmp
+	jeq		sys_commands
+	
+	lri		$acc0.m,#0xface
+	cmp
+	jne		wait_commands
+	
+	lrs		$acc1.m,@CMBL
+	
+	cmpi	$acc1.m,#0x0010
+	jeq		process_first_voice
+	
+	cmpi	$acc1.m,#0x0020
+	jeq		process_next_voice
+	
+	cmpi	$acc1.m,#0x0080
+	jeq		get_pb_address
+	
+	cmpi	$acc1.m,#0x0100
+	jeq		send_samples
+	
+	cmpi	$acc1.m,#0xdead
+	jeq		task_terminate
+	
+	jmp		recv_cmd
+	
+sys_commands:
+	lrs		$acc1.m,@CMBL
+	
+	cmpi	$acc1.m,#0x0001
+	jeq		run_nexttask
+	
+	cmpi	$acc1.m,#0x0002
+	jeq		0x8000
+	halt
+	
+wait_commands:
+	jmp		recv_cmd
+	
+run_nexttask:
+	s16
+	clr			$acc0
+	clr			$acc1
+	call		wait_mail_acc0
+	lrs			$acc0.l,@CMBL
+	call		wait_mail_acc1
+	lrs			$acc1.l,@CMBL
+	call		wait_mail_acc1
+	lrs			$acc1.m,@CMBL
+	srs			@DMAMMEMH,$acc0.m
+	srs			@DMAMMEML,$acc0.l
+	si			@DMACR,#DMA_TO_CPU
+	srs			@DMADSPM,$acc1.m
+	srs			@DMABLEN,$acc1.l
+	clr			$acc0
+	clr			$acc1
+	call		wait_mail_acc0
+	lrs			$acc0.l,@CMBL
+	mrr			$ix0,$acc0.m
+	mrr			$ix1,$acc0.l
+	call		wait_mail_acc1
+	lrs			$acc1.l,@CMBL
+	call		wait_mail_acc1
+	lrs			$acc1.m,@CMBL
+	mrr			$ix2,$acc1.m
+	mrr			$ix3,$acc1.l
+	clr			$acc0
+	call		wait_mail_acc0
+	lrs			$acc0.m,@CMBL
+	mrr			$ar0,$acc0.m
+	clr			$acc1
+	call		wait_mail_acc1
+	lrs			$acx0.l,@CMBL
+	mrr			$acx0.h,$acc1.m
+	call		wait_mail_acc0
+	lrs			$acx1.l,@CMBL
+	call		wait_mail_acc0
+	lrs			$acx1.h,@CMBL
+wait_dmem_dma:
+	lrs			$acc0.m,@DMACR
+	andf		$acc0.m,#0x0004
+	jlnz		wait_dmem_dma
+	jmp			0x80b5		
+	halt
+
+task_terminate:
+	si		@DMBH,#0xdcd1
+	si		@DMBL,#0x0003
+	si		@DIRQ,#0x01
+	jmp		recv_cmd
+
+send_samples:
+	lri		$ar0,#MEM_SND_BUF
+	lris	$acx1.l,#DMA_TO_CPU
+	lri		$acx0.l,#NUM_SAMPLES*4
+	lr		$acc0.m,@OUTBUF_ADDRH
+	lr		$acc0.l,@OUTBUF_ADDRL
+	call	dma_data
+	
+	si		@DMBH,#0xdcd1
+	si		@DMBL,#0x0004
+	si		@DIRQ,#0x01
+	
+	jmp		recv_cmd
+
+get_pb_address:
+	call	wait_mail_recv
+	
+	lri		$ar2,#PB_MADDRH
+	
+	lrs		$acc0.m,@CMBH
+	srri	@$ar2,$acc0.m
+	
+	lrs		$acc0.m,@CMBL
+	srr		@$ar2,$acc0.m
+
+	jmp		recv_cmd
+	
+process_first_voice:
+	si		@DMACR,#DMA_TO_DSP
+	call	dma_pb_block
+	
+	lri		$ar0,#MEM_SND_BUF
+	lri		$acc1.l,#0
+	
+	lri		$acx0.l,#NUM_SAMPLES*2
+	loop	$acx0.l
+	srri	@$ar0,$acc1.l
+
+	jmp		dsp_mixer
+
+process_next_voice:
+	si		@DMACR,#DMA_TO_DSP
+	call	dma_pb_block
+	
+dsp_mixer:
+	clr		$acc0
+
+	lr		$acc1.m,@FLAGS_SMPL
+	andcf	$acc1.m,#VOICE_FLAGL_PAUSE
+	jlz		finish_voice
+	
+	lri		$ar1,#SNDBUF_SADDRH
+	lrri	$acc0.m,@$ar1
+	lrrd	$acc0.l,@$ar1
+	
+	tst		$acc0
+	jne		no_change_buffer
+
+	lr		$acc1.m,@FLAGS_SMPH
+	ori		$acc1.m,#VOICE_FLAGH_CB
+	sr		@FLAGS_SMPH,$acc1.m
+
+	jmp		finish_voice
+
+no_change_buffer:
+	lr		$acc1.l,@FLAGS_SMPL
+	
+	mrr		$acc1.m,$acc1.l
+	andi	$acc1.m,#0x02
+	addi	$acc1.m,#select_format
+	mrr		$ar3,$acc1.m
+	ilrri	$acc0.m,@$ar3
+	ilrri	$acc1.m,@$ar3
+	call	setup_accl
+	
+	mrr		$acc1.m,$acc1.l
+	andi	$acc1.m,#0x03
+	addi	$acc1.m,#select_mixer
+	mrr		$ar3,$acc1.m
+	ilrr	$acc1.m,@$ar3
+	mrr		$ar3,$acc1.m
+
+	lri		$ar1,#SMP_L
+	clr'l	$acc0 : $acx0.h,@$ar1		//left last sample
+	lrri	$acx1.h,@$ar1		//right last sample
+	lrri	$ix2,@$ar1
+	lrri	$ix3,@$ar1
+	lrri	$acc0.h,@$ar1
+	lrri	$acc0.m,@$ar1
+		
+	lri		$ar0,#MEM_SND_BUF
+	lri		$acx0.l,#NUM_SAMPLES
+
+	lri		$ar1,#FREQ_SMPH
+	lri		$wr1,#0x0004
+		
+	tst		$acc0
+	jeq		no_delay
+	
+	lri		$ix0,#2
+
+	bloop	$acx0.l,delay_loop
+	decm	$acc0.m
+	jeq		exit_delay
+delay_loop:
+	addarn	$ar0,$ix0
+
+exit_delay:
+	mrr		$acx0.l,$st3
+	sr		@DELAY_SMPH,$acc0.h
+	sr		@DELAY_SMPL,$acc0.m
+	
+no_delay:
+	s40
+	bloop	$acx0.l,dspmixer_loop_end
+	
+	// right/left channel sample mix
+	lrri	$acc1.m,@$ar0		// right channel
+	lrr		$acc0.m,@$ar0		// left channel
+	addr'dr	$acc1.m,$acx1.h : $ar0
+	addr's	$acc0.m,$acx0.h : @$ar0,$acc1.m
+	srri	@$ar0,$acc0.m
+
+	clr'l	$acc0 : $acx1.h,@$ar1
+	lrri	$acx1.l,@$ar1
+	lrr		$acc0.l,@$ar1
+
+
+	addax	$acc0,$acx1
+	mrr		$acx0.l,$acc0.m
+	srri	@$ar1,$acc0.l
+	cmpis	$acc0.m,#DEF_FREQ_INT
+	jlt		no_mix
+	
+	jmpr	$ar3
+		
+mono_8bits:
+	bloop	$acx0.l,mono_8bits_end
+	lrs		$acc1.m,@ACDAT			//right channel
+mono_8bits_end:
+	mrr		$acc0.m,$acc1.m			//left channel
+	
+	jmp		mix_samples
+
+stereo_8bits:
+	bloop	$acx0.l,stereo_8bits_end
+	lrs		$acc1.m,@ACDAT			//right channel
+stereo_8bits_end:
+	lrs		$acc0.m,@ACDAT			//left channel
+
+	jmp		mix_samples
+	
+mono_16bits:
+	bloop	$acx0.l,mono_16bits_end
+	lrs		$acc1.m,@ACDAT			//right channel
+mono_16bits_end:
+	mrr		$acc0.m,$acc1.m			//left channel
+
+	jmp		mix_samples
+	
+stereo_16bits:
+	bloop	$acx0.l,stereo_16bits_end
+	lrs		$acc1.m,@ACDAT			//right channel
+stereo_16bits_end:
+	lrs		$acc0.m,@ACDAT			//left channel
+
+mix_samples:
+	// multiply samples*volume
+	mrr		$acx0.h,$ix2
+	mulc	$acc0.m,$acx0.h			//left channel
+	mrr		$acx1.h,$ix3
+	mulcmv	$acc1.m,$acx1.h,$acc0	//right channel
+	asl		$acc0,#8
+	movp's	$acc1 : @$ar1,$acc0.m
+	asl		$acc1,#8
+	srrd	@$ar1,$acc1.m
+no_mix:
+	lrri	$acx0.h,@$ar1
+dspmixer_loop_end:
+	lrri	$acx1.h,@$ar1
+				
+mixer_end:
+	s16
+	
+	lri		$wr1,#0xffff
+	
+	lri		$ar1,#PDS
+	lrs		$acc1.m,@ACPDS
+	srrd	@$ar1,$acc1.m
+	lrs		$acc1.m,@ACYN2
+	srrd	@$ar1,$acc1.m
+	lrs		$acc1.m,@ACYN1
+	srrd	@$ar1,$acc1.m
+	lrs		$acc1.m,@ACCAL
+	srrd	@$ar1,$acc1.m
+	lrs		$acc1.m,@ACCAH
+	srrd	@$ar1,$acc1.m
+
+finish_voice:
+	lr		$acc1.m,@FLAGS_SMPH
+	ori		$acc1.m,#VOICE_FLAGH_END
+	sr		@FLAGS_SMPH,$acc1.m
+	
+	si		@DMACR,#DMA_TO_CPU
+	call	dma_pb_block
+
+	si		@DMBH,#0xdcd1
+	si		@DMBL,#0x0004
+	si		@DIRQ,#0x01
+
+	jmp		recv_cmd
+	
+dma_pb_block:
+	clr		$acc0
+	lr		$acc0.m,@PB_MADDRH
+	lr		$acc0.l,@PB_MADDRL
+	srs		@DMAMMEMH,$acc0.m
+	srs		@DMAMMEML,$acc0.l
+	si		@DMADSPM,#PB_ADDR
+	si		@DMABLEN,#PB_STURCT_SIZE
+
+wait_dma_pb:
+	lrs		$acc0.m,@DMACR
+	andf	$acc0.m,#0x04
+	jlnz	wait_dma_pb
+	ret
+
+dma_data:
+	srs		@DMAMMEMH,$acc0.m
+	srs		@DMAMMEML,$acc0.l
+	sr		@DMADSPM,$ar0
+	sr		@DMACR,$acx1.l
+	sr		@DMABLEN,$acx0.l
+	
+wait_dma:
+	lrs		$acc1.m,@DMACR
+	andf	$acc1.m,#0x04
+	jlnz	wait_dma
+	ret
+		
+//setup_accl: acc0.m = format, acc1.m = gain, ar1 = sndbuf_start
+setup_accl:
+	srs		@ACFMT,$acc0.m
+	srs		@ACGAN,$acc1.m
+
+	lrri	$acc0.m,@$ar1
+	srs		@ACSAH,$acc0.m
+	lrri	$acc0.m,@$ar1
+	srs		@ACSAL,$acc0.m
+	lrri	$acc0.m,@$ar1
+	srs		@ACEAH,$acc0.m
+	lrri	$acc0.m,@$ar1
+	srs		@ACEAL,$acc0.m
+	lrri	$acc0.m,@$ar1
+	srs		@ACCAH,$acc0.m
+	lrri	$acc0.m,@$ar1
+	srs		@ACCAL,$acc0.m
+	lrri	$acc0.m,@$ar1
+	srs		@ACYN1,$acc0.m
+	lrri	$acc0.m,@$ar1
+	srs		@ACYN2,$acc0.m
+	lrri	$acc0.m,@$ar1
+	srs		@ACPDS,$acc0.m
+
+	si		@ACCOEF+0,#0
+	si		@ACCOEF+1,#0
+	si		@ACCOEF+2,#0
+	si		@ACCOEF+3,#0
+	si		@ACCOEF+4,#0
+	si		@ACCOEF+5,#0
+	si		@ACCOEF+6,#0
+	si		@ACCOEF+7,#0
+	si		@ACCOEF+8,#0
+	si		@ACCOEF+9,#0
+	si		@ACCOEF+10,#0
+	si		@ACCOEF+11,#0
+	si		@ACCOEF+12,#0
+	si		@ACCOEF+13,#0
+	si		@ACCOEF+14,#0
+	si		@ACCOEF+15,#0
+
+	ret
+	
+wait_mail_sent:
+	lrs		$acc0.m,@DMBH
+	andf	$acc0.m,#0x8000
+	jlnz	wait_mail_sent
+	ret
+	
+wait_mail_recv:
+	lrs		$acc1.m,@CMBH
+	andcf	$acc1.m,#0x8000
+	jlnz	wait_mail_recv
+	ret
+	
+wait_mail_acc0:
+	lrs		$acc0.m,@CMBH
+	andcf	$acc0.m,#0x8000
+	jlnz	wait_mail_acc0
+	ret
+	
+wait_mail_acc1:
+	lrs		$acc1.m,@CMBH
+	andcf	$acc1.m,#0x8000
+	jlnz	wait_mail_recv
+	ret
+
+/* exceptions */	
+exception1:		// Stack overflow
+	rti
+	
+exception2:
+	rti
+	
+exception3:
+	rti
+	
+exception4:
+	rti
+
+exception5:		// Accelerator address overflow
+	s16
+	mrr		$st1,$acc0.l
+	mrr		$st1,$acc0.m
+
+	clr		$acc0
+
+	lrs		$acc0.m,@ACYN1
+	srs		@ACYN1,$acc0.m
+	lrs		$acc0.m,@ACYN2
+	srs		@ACYN2,$acc0.m
+	lrs		$acc0.m,@ACPDS
+	srs		@ACPDS,$acc0.m
+
+	lr		$acc0.m,@FLAGS_SMPH
+	ori		$acc0.m,#VOICE_FLAGH_CB
+	sr		@FLAGS_SMPH,$acc0.m
+
+	mrr		$acc0.m,$st1
+	mrr		$acc0.l,$st1
+	rti
+	
+exception6:
+	rti
+	
+exception7:		// External interrupt (message from CPU)
+	rti
+
+select_mixer:
+	cw		mono_8bits
+	cw		stereo_8bits
+	cw		mono_16bits
+	cw		stereo_16bits
+
+select_format:
+	cw		ACCL_FMT_8BIT
+	cw		ACCL_GAIN_8BIT
+	cw		ACCL_FMT_16BIT
+	cw		ACCL_GAIN_16BIT
+	
