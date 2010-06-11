76c351723749987b967ae3d7a2ddb9a54fd23d8d
tantricity <tantricity@users.sourceforge.net>
2010-06-11 00:32:13 +0000

    missing usbgecko defs

--- gc/ogc/usbgecko.h
+++ gc/ogc/usbgecko.h
@@ -13,6 +13,10 @@ int usb_recvbuffer(s32 chn,void *buffer,int size);
 int usb_sendbuffer(s32 chn,const void *buffer,int size);
 int usb_recvbuffer_safe(s32 chn,void *buffer,int size);
 int usb_sendbuffer_safe(s32 chn,const void *buffer,int size);
+int usb_recvbuffer_ex(s32 chn,void *buffer,int size, int retries);
+int usb_sendbuffer_ex(s32 chn,const void *buffer,int size, int retries);
+int usb_recvbuffer_safe_ex(s32 chn,void *buffer,int size, int retries);
+int usb_sendbuffer_safe_ex(s32 chn,const void *buffer,int size, int retries);
 
 #ifdef __cplusplus
    }
