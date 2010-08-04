6fd120b7491bf9f6b9b6321387bd382a241b7a8f
Michael Wiedenbauer <shagkur@users.sourceforge.net>
2010-08-04 10:53:04 +0000

    - Added c_guMtxInvXpose & defines to guMtxInvXpose - properly renamed PS variant.

--- libogc/gu.c
+++ libogc/gu.c
@@ -478,6 +478,56 @@ void c_guMtxTranspose(Mtx src,Mtx xPose)
         c_guMtxCopy(mTmp,xPose);
 }
 
+u32 c_guMtxInvXpose(Mtx src, Mtx xPose)
+{
+    Mtx mTmp;
+    MtxP m;
+    f32 det;
+
+    if(src == xPose)
+        m = mTmp;
+    else
+        m = xPose;
+
+    // Compute the determinant of the upper 3x3 submatrix
+    det =   src[0][0]*src[1][1]*src[2][2] + src[0][1]*src[1][2]*src[2][0] + src[0][2]*src[1][0]*src[2][1]
+          - src[2][0]*src[1][1]*src[0][2] - src[1][0]*src[0][1]*src[2][2] - src[0][0]*src[2][1]*src[1][2];
+
+    // Check if matrix is singular
+    if(det == 0.0f) return 0;
+
+    // Compute the inverse of the upper submatrix:
+
+    // Find the transposed matrix of cofactors of the upper submatrix
+    // and multiply by (1/det)
+    
+    det = 1.0f / det;
+
+    m[0][0] =  (src[1][1]*src[2][2] - src[2][1]*src[1][2]) * det;
+    m[0][1] = -(src[1][0]*src[2][2] - src[2][0]*src[1][2]) * det;
+    m[0][2] =  (src[1][0]*src[2][1] - src[2][0]*src[1][1]) * det;
+
+    m[1][0] = -(src[0][1]*src[2][2] - src[2][1]*src[0][2]) * det;
+    m[1][1] =  (src[0][0]*src[2][2] - src[2][0]*src[0][2]) * det;
+    m[1][2] = -(src[0][0]*src[2][1] - src[2][0]*src[0][1]) * det;
+
+    m[2][0] =  (src[0][1]*src[1][2] - src[1][1]*src[0][2]) * det;
+    m[2][1] = -(src[0][0]*src[1][2] - src[1][0]*src[0][2]) * det;
+    m[2][2] =  (src[0][0]*src[1][1] - src[1][0]*src[0][1]) * det;
+
+
+    // The 4th columns should be zero
+    m[0][3] = 0.0F;
+    m[1][3] = 0.0F;
+    m[2][3] = 0.0F;
+
+    // Copy back if needed
+    if(m == mTmp)
+        c_guMtxCopy(mTmp, xPose);
+
+    return 1;
+}
+
 void c_guMtxReflect(Mtx m,guVector *p,guVector *n)
 {
     f32 vxy, vxz, vyz, pdotn;
