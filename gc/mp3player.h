1d7fdfefa500ae87035ce67d64c1d3743d07c943
Lioncash <mathew1800@gmail.com>
2018-04-19 18:55:42 -0400

    mp3player: Amend function prototypes
    This also corrects the function prototype of DataTransferCallback() which should have been specified with an s32
    parameter representing a voice, rather than an empty parameter.

--- gc/mp3player.h
+++ gc/mp3player.h
@@ -8,9 +8,9 @@
    extern "C" {
 #endif /* __cplusplus */
 
-void MP3Player_Init();
-void MP3Player_Stop();
-BOOL MP3Player_IsPlaying();
+void MP3Player_Init(void);
+void MP3Player_Stop(void);
+BOOL MP3Player_IsPlaying(void);
 void MP3Player_Volume(u32 volume);
 s32 MP3Player_PlayBuffer(const void *buffer,s32 len,void (*filterfunc)(struct mad_stream *,struct mad_frame *));
 s32 MP3Player_PlayFile(void *cb_data,s32 (*reader)(void *,void *,s32),void (*filterfunc)(struct mad_stream *,struct mad_frame *));
