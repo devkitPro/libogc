10b145a08381d8209efb7a795280117e13ece2f4
Dave Murphy <WinterMute@users.noreply.github.com>
2020-03-03 14:10:07 +0000

    fix _SHIFTL macro usage

--- libogc/audio.c
+++ libogc/audio.c
@@ -399,7 +399,7 @@ u32 AUDIO_GetDMABytesLeft(void)
 
 u32 AUDIO_GetDMAStartAddr(void)
 {
-	return (_SHIFTL((_dspReg[24],16,13)|(_dspReg[25]&0xffe0));
+	return _SHIFTL(_dspReg[24],16,13) | (_dspReg[25]&0xffe0);
 }
 
 u32 AUDIO_GetDMALength(void)
