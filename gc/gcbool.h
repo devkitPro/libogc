dab21398df9a56442b274d21d87eba9c7b0f5da6
Dave Murphy <davem@devkitpro.org>
2023-05-28 11:31:13 +0100

    deprecate BOOL

---
+++ gc/gcbool.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef bool BOOL;
+
+__attribute__((deprecated)) static const bool FALSE = false;
+__attribute__((deprecated)) static const bool TRUE  = true;
+
