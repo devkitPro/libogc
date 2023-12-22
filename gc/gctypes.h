dab81d801174e08b846cffe4531fe0325d1f5f8c
Crayon <crayon1@rocketmail.com>
2023-12-22 12:44:09 -0500

    Fix typo (#162)
    thanks for the PR of the typo :)

--- gc/gctypes.h
+++ gc/gctypes.h
@@ -62,7 +62,7 @@ typedef volatile double vf64;
 /*+----------------------------------------------------------------------------------------------+*/
 #ifndef BIG_ENDIAN
 #define BIG_ENDIAN     1234
-#endif /* BIGE_ENDIAN */
+#endif /* BIG_ENDIAN */
 /*+----------------------------------------------------------------------------------------------+*/
 #ifndef BYTE_ORDER
 #define BYTE_ORDER     BIG_ENDIAN
