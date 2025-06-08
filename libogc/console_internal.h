49162d1a950c8ad5e468a3376b78819b341b4e2f
Dave Murphy <WinterMute@users.noreply.github.com>
2025-06-08 18:47:17 +0100

    Console: improvements by porting code from libctru (#220)
    * console improvements ported from libctru : extends ansi escape code support and allows codes to be spread across multiple prints
    
    * console: implement 90 - 97 (bright fg) and 100 - 107 (bright bg)
    
    * prefer console.h to consol.h
    
    * simplify console init
    
    * provide defines for base console colors

--- libogc/console_internal.h
+++ libogc/console_internal.h
@@ -1,6 +1,19 @@
 #ifndef __CONSOLE_INTERNAL_H__
 #define __CONSOLE_INTERNAL_H__
 
+#define CONSOLE_COLOR_BOLD      (1<<0)
+#define CONSOLE_COLOR_FAINT     (1<<1)
+#define CONSOLE_ITALIC          (1<<2)
+#define CONSOLE_UNDERLINE       (1<<3)
+#define CONSOLE_BLINK_SLOW      (1<<4)
+#define CONSOLE_BLINK_FAST      (1<<5)
+#define CONSOLE_COLOR_REVERSE   (1<<6)
+#define CONSOLE_CONCEAL         (1<<7)
+#define CONSOLE_CROSSED_OUT     (1<<8)
+#define CONSOLE_FG_CUSTOM       (1<<9)
+#define CONSOLE_BG_CUSTOM       (1<<10)
+#define CONSOLE_COLOR_FG_BRIGHT (1<<11)
+#define CONSOLE_COLOR_BG_BRIGHT (1<<12)
 
 #define FONT_XSIZE		8
 #define FONT_YSIZE		16
@@ -15,11 +28,12 @@ typedef struct _console_data_s {
 	unsigned char *font;
 	int con_xres,con_yres,con_stride;
 	int target_x,target_y, tgt_stride;
-	int cursor_row,cursor_col;
-	int saved_row,saved_col;
+	int cursorX,cursorY;
+	int prevCursorX,prevCursorY;
 	int con_rows, con_cols;
-
-	unsigned int foreground,background;
+	int tabSize;
+	unsigned int fg,bg;
+	unsigned int flags;
 } console_data_s;
 
 extern int __console_write(struct _reent *r,void *fd,const char *ptr,size_t len);
