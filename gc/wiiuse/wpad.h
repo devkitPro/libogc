devkitPro/libogc

commit ca84f9d7e7b21d6ea2668759e943fef1a91339e5

Wiimote: Add wireless controller syncing functionality (#197)
* Add remote syncing
* replicating Wii menu pairing functionality
* Properly wait for USB to close
* Implement Guest mode pairing
* General disconnect CB
* Disambiguate front panel power button from auth'd Wiimote power button

---------

Co-authored-by: DacoTaco <daco_65@hotmail.com>

gc/wiiuse/wpad.h
@@ -43,8 +43,12 @@ enum {
 	WPAD_CHAN_2,
 	WPAD_CHAN_3,
 	WPAD_BALANCE_BOARD,
-	WPAD_MAX_WIIMOTES,
+	WPAD_CHAN_UNKNOWN,
+	WPAD_MAX_DEVICES,
 };
+
+// Compatibility with old apps
+#define WPAD_MAX_WIIMOTES WPAD_MAX_DEVICES
 											
 #define WPAD_BUTTON_2							0x0001
 #define WPAD_BUTTON_1							0x0002
@@ -136,6 +140,14 @@ enum {
 #define WPAD_THRESH_DEFAULT_BALANCEBOARD		60
 #define WPAD_THRESH_DEFAULT_MOTION_PLUS			100
 
+#define WPAD_DISCON_AUTH_FAILURE				0x05	/* HCI_AUTHENTICATION_FAILURE */
+#define WPAD_DISCON_TIMEOUT						0x08	/* HCI_CONN_TIMEOUT */
+#define WPAD_DISCON_SYNC_PRESSED				0x13	/* HCI_OTHER_END_TERMINATED_CONN_USER_ENDED */
+#define WPAD_DISCON_BATTERY_DIED				0x14	/* HCI_OTHER_END_TERMINATED_CONN_LOW_RESOURCES */
+#define WPAD_DISCON_POWER_OFF					0x15	/* HCI_OTHER_END_TERMINATED_CONN_ABOUT_TO_POWER_OFF */
+#define WPAD_DISCON_IDLE_TIMEOUT				0x16	/* HCI_CONN_TERMINATED_BY_LOCAL_HOST */
+#define WPAD_DISCON_REPEATED_ATTEMPTS			0x17	/* HCI_REPEATED_ATTEMPTS */
+
 #ifdef __cplusplus
    extern "C" {
 #endif /* __cplusplus */
@@ -166,6 +178,9 @@ typedef struct _wpad_encstatus
 
 typedef void (*WPADDataCallback)(s32 chan, const WPADData *data);
 typedef void (*WPADShutdownCallback)(s32 chan);
+typedef void (*WPADDisconnectCallback)(s32 chan, u8 reason);
+typedef void (*WPADStatusCallback)(s32 chan);
+typedef void (*WPADHostSyncBtnCallback)(u32 held);
 
 s32 WPAD_Init(void);
 s32 WPAD_ControlSpeaker(s32 chan,s32 enable);
@@ -182,10 +197,17 @@ s32 WPAD_SetEventBufs(s32 chan, WPADData *bufs, u32 cnt);
 s32 WPAD_Disconnect(s32 chan);
 s32 WPAD_IsSpeakerEnabled(s32 chan);
 s32 WPAD_SendStreamData(s32 chan,void *buf,u32 len);
-void WPAD_Shutdown(void);
+s32 WPAD_Search(void);
+s32 WPAD_StopSearch(void);
+s32 WPAD_StartPairing(void);
+s32 WPAD_WipeSavedControllers(void);
+s32 WPAD_Shutdown(void);
 void WPAD_SetIdleTimeout(u32 seconds);
 void WPAD_SetPowerButtonCallback(WPADShutdownCallback cb);
 void WPAD_SetBatteryDeadCallback(WPADShutdownCallback cb);
+void WPAD_SetDisconnectCallback(WPADDisconnectCallback cb);
+void WPAD_SetHostSyncButtonCallback(WPADHostSyncBtnCallback cb);
+void WPAD_SetStatusCallback(WPADStatusCallback cb);
 s32 WPAD_ScanPads(void);
 s32 WPAD_Rumble(s32 chan, int status);
 s32 WPAD_SetIdleThresholds(s32 chan, s32 btns, s32 ir, s32 accel, s32 js, s32 wb, s32 mp);
@@ -200,6 +222,8 @@ void WPAD_Orientation(int chan, struct orient_t *orient);
 void WPAD_GForce(int chan, struct gforce_t *gforce);
 void WPAD_Accel(int chan, struct vec3w_t *accel);
 void WPAD_Expansion(int chan, struct expansion_t *exp);
+void WPAD_PadStatus(int chan);
+bool WPAD_IsBatteryCritical(int chan);
 
 #ifdef __cplusplus
    }

gc/bte/bd_addr.h
@@ -39,11 +39,11 @@
    extern "C" {
 #endif /* __cplusplus */
 
-struct bd_addr {
-  u8 addr[6];
-};
+#define BD_ADDR_LEN	6
 
-#define BD_ADDR_LEN			6
+struct bd_addr {
+  u8 addr[BD_ADDR_LEN];
+};
 
 #define BD_ADDR_ANY			(&(struct bd_addr){{0,0,0,0,0,0}})
 #define BD_ADDR_LOCAL		(&(struct bd_addr){{0,0,0,0xff,0xff,0xff}})
@@ -55,6 +55,23 @@ struct bd_addr {
 				        (bdaddr)->addr[3] = d; \
 				        (bdaddr)->addr[4] = e; \
 						(bdaddr)->addr[5] = f; }while(0)
+
+#define BD_ADDR_FROM_BYTES(bdaddr, bytes) do{ \
+                                        (bdaddr)->addr[0] = bytes[5]; \
+				        (bdaddr)->addr[1] = bytes[4]; \
+				        (bdaddr)->addr[2] = bytes[3]; \
+				        (bdaddr)->addr[3] = bytes[2]; \
+				        (bdaddr)->addr[4] = bytes[1]; \
+						(bdaddr)->addr[5] = bytes[0]; }while(0)
+
+#define BYTES_FROM_BD_ADDR(bytes, bdaddr) do{ \
+                                        bytes[0] = (bdaddr)->addr[5]; \
+				        bytes[1] = (bdaddr)->addr[4]; \
+				        bytes[2] = (bdaddr)->addr[3]; \
+				        bytes[3] = (bdaddr)->addr[2]; \
+				        bytes[4] = (bdaddr)->addr[1]; \
+				        bytes[5] = (bdaddr)->addr[0]; }while(0)
+
 //TODO: USE memcmp????
 #define bd_addr_cmp(addr1, addr2) (((addr1)->addr[0] == (addr2)->addr[0]) && \
 				   ((addr1)->addr[1] == (addr2)->addr[1]) && \

gc/bte/bte.h
@@ -56,18 +56,27 @@
 #define HIDP_CTRL_VC_UNPLUG			0x05
 
 /* HIDP data transaction headers */
-#define HIDP_DATA_RTYPE_MASK        0x03
-#define HIDP_DATA_RSRVD_MASK        0x0c
-#define HIDP_DATA_RTYPE_OTHER       0x00
-#define HIDP_DATA_RTYPE_INPUT       0x01
-#define HIDP_DATA_RTYPE_OUPUT       0x02
-#define HIDP_DATA_RTYPE_FEATURE     0x03
+#define HIDP_DATA_RTYPE_MASK		0x03
+#define HIDP_DATA_RSRVD_MASK		0x0c
+#define HIDP_DATA_RTYPE_OTHER		0x00
+#define HIDP_DATA_RTYPE_INPUT		0x01
+#define HIDP_DATA_RTYPE_OUPUT		0x02
+#define HIDP_DATA_RTYPE_FEATURE		0x03
 
 #define HIDP_PROTO_BOOT				0x00
 #define HIDP_PROTO_REPORT			0x01
 
+#define BD_LINK_KEY_LEN				16
+#define BD_NAME_LEN					248
+#define BD_MAX_INQUIRY_DEVS			255
+
+enum pair_mode {
+	PAIR_MODE_NORMAL,
+	PAIR_MODE_TEMPORARY
+};
+
 #ifdef __cplusplus
-   extern "C" {
+	extern "C" {
 #endif /* __cplusplus */
 
 struct l2cap_pcb;
@@ -88,10 +97,22 @@ struct inquiry_info_ex
 	u16 co;
 };
 
+struct bte_inquiry_res
+{
+	u8 count;
+	struct inquiry_info_ex *info;
+};
+
 struct linkkey_info
 {
 	struct bd_addr bdaddr;
-	u8 key[16];
+	u8 key[BD_LINK_KEY_LEN];
+};
+
+struct pad_name_info
+{
+	struct bd_addr bdaddr;
+	u8 name[BD_NAME_LEN];
 };
 
 struct bte_pcb
@@ -112,6 +133,7 @@ struct bte_pcb
 
 
 	s32 (*recv)(void *arg,void *buffer,u16 len);
+	s32 (*conn_req)(void *arg,struct bte_pcb *pcb,struct bd_addr *bdaddr,u8 *cod,u8 link_type,u8 err);
 	s32 (*conn_cfm)(void *arg,struct bte_pcb *pcb,u8 err);
 	s32 (*disconn_cfm)(void *arg,struct bte_pcb *pcb,u8 err);
 };
@@ -120,34 +142,47 @@ typedef s32 (*btecallback)(s32 result,void *userdata);
 
 void BTE_Init(void);
 void BTE_Shutdown(void);
+void BTE_Close(void);
 s32 BTE_InitCore(btecallback cb);
 s32 BTE_ApplyPatch(btecallback cb);
 s32 BTE_InitSub(btecallback cb);
 s32 BTE_ReadStoredLinkKey(struct linkkey_info *keys,u8 max_cnt,btecallback cb);
 s32 BTE_ReadBdAddr(struct bd_addr *bdaddr, btecallback cb);
+s32 BTE_SetEvtFilter(u8 filter_type,u8 filter_cond_type,u8 *cond, btecallback cb);
+s32 BTE_ReadRemoteName(struct pad_name_info *padinfo, btecallback cb);
+s32 BTE_Inquiry(u8 max_cnt,u8 flush, btecallback cb);
+s32 BTE_PeriodicInquiry(u8 max_cnt,u8 flush,btecallback cb);
+s32 BTE_ExitPeriodicInquiry(void);
+s32 BTE_LinkKeyRequestReply(struct bd_addr *bdaddr,u8 *key);
+s32 BTE_LinkKeyRequestNegativeReply(struct bd_addr *bdaddr);
 void (*BTE_SetDisconnectCallback(void (*callback)(struct bd_addr *bdaddr,u8 reason)))(struct bd_addr *bdaddr,u8 reason);
+void BTE_SetHostSyncButtonCallback(void (*callback)(u32 held));
+void BTE_SetConnectionRequestCallback(s8 (*callback)(void *arg,struct bd_addr *bdaddr,u8 *cod,u8 link_type));
+void BTE_SetLinkKeyRequestCallback(s8 (*callback)(void *arg,struct bd_addr *bdaddr));
+void BTE_SetLinkKeyNotificationCallback(s8 (*callback)(void *arg,struct bd_addr *bdaddr,u8 *key));
+u8 BTE_GetPairMode(void);
+s32 BTE_WriteStoredLinkKey(struct bd_addr *bdaddr,u8 *key);
+s32 BTE_ClearStoredLinkKeys(void);
+s32 BTE_DeleteStoredLinkKey(struct bd_addr *bdaddr);
 
 struct bte_pcb* bte_new(void);
+void bte_free(struct bte_pcb *pcb);
 void bte_arg(struct bte_pcb *pcb,void *arg);
 void bte_received(struct bte_pcb *pcb, s32 (*recv)(void *arg,void *buffer,u16 len));
 void bte_disconnected(struct bte_pcb *pcb,s32 (disconn_cfm)(void *arg,struct bte_pcb *pcb,u8 err));
 
-s32 bte_registerdeviceasync(struct bte_pcb *pcb,struct bd_addr *bdaddr,s32 (*conn_cfm)(void *arg,struct bte_pcb *pcb,u8 err));
-
 s32 bte_disconnect(struct bte_pcb *pcb);
 
-//s32 bte_listen(struct bte_pcb *pcb,struct bd_addr *bdaddr,u8 psm);
-//s32 bte_accept(struct bte_pcb *pcb,s32 (*recv)(void *arg,void *buffer,u16 len));
-s32 bte_inquiry(struct inquiry_info *info,u8 max_cnt,u8 flush);
-s32 bte_inquiry_ex(struct inquiry_info_ex *info,u8 max_cnt,u8 flush);
-//s32 bte_connect(struct bte_pcb *pcb,struct bd_addr *bdaddr,u8 psm,s32 (*recv)(void *arg,void *buffer,u16 len));
-//s32 bte_connect_ex(struct bte_pcb *pcb,struct inquiry_info_ex *info,u8 psm,s32 (*recv)(void *arg,void *buffer,u16 len));
+s32 bte_listenasync(struct bte_pcb *pcb,struct bd_addr *bdaddr,s32 (*conn_cfm)(void *arg,struct bte_pcb *pcb,u8 err));
+s32 bte_listenasync_step2(struct bte_pcb *pcb,s32 (*conn_cfm)(void *arg,struct bte_pcb *pcb,u8 err));
+s32 bte_connectasync(struct bte_pcb *pcb,struct bd_addr *bdaddr,s32 (*conn_cfm)(void *arg,struct bte_pcb *pcb,u8 err));
+s32 bte_connectasync_step2(struct bte_pcb *pcb,s32 (*conn_cfm)(void *arg,struct bte_pcb *pcb,u8 err));
 s32 bte_senddata(struct bte_pcb *pcb,void *message,u16 len);
 s32 bte_sendmessage(struct bte_pcb *pcb,void *message,u16 len);
 s32 bte_sendmessageasync(struct bte_pcb *pcb,void *message,u16 len,s32 (*sent)(void *arg,struct bte_pcb *pcb,u8 err));
 
 #ifdef __cplusplus
-   }
+	}
 #endif /* __cplusplus */
 
 #endif

gc/ogc/conf.h
@@ -128,6 +128,8 @@ enum {
 
 #define CONF_PAD_MAX_REGISTERED 10
 #define CONF_PAD_MAX_ACTIVE 4
+#define CONF_PAD_ACTIVE_AND_OTHER (CONF_PAD_MAX_ACTIVE + 2)
+#define CONF_PAD_TOTAL (CONF_PAD_MAX_REGISTERED + CONF_PAD_ACTIVE_AND_OTHER)
 
 typedef struct _conf_pad_device conf_pad_device;
 
@@ -146,7 +148,24 @@ struct _conf_pads {
 	conf_pad_device unknown;
 } ATTRIBUTE_PACKED;
 
+typedef struct _conf_pad_guest_device conf_pad_guest_device;
+
+struct _conf_pad_guest_device {
+	u8 bdaddr[6];
+	char name[0x40];
+	u8 link_key[16];
+} ATTRIBUTE_PACKED;
+
+typedef struct _conf_pad_guests conf_pad_guests;
+
+struct _conf_pad_guests {
+	u8 num_guests;
+	conf_pad_guest_device guests[CONF_PAD_MAX_ACTIVE];
+	conf_pad_guest_device unknown[2]; // Balance Board can't be set as guest...
+} ATTRIBUTE_PACKED;
+
 s32 CONF_Init(void);
+s32 CONF_SaveChanges(void);
 s32 CONF_GetLength(const char *name);
 s32 CONF_GetType(const char *name);
 s32 CONF_Get(const char *name, void *buffer, u32 length);
@@ -164,6 +183,9 @@ s32 CONF_GetCounterBias(u32 *bias);
 s32 CONF_GetScreenSaverMode(void);
 s32 CONF_GetDisplayOffsetH(s8 *offset);
 s32 CONF_GetPadDevices(conf_pads *pads);
+s32 CONF_SetPadDevices(const conf_pads *pads);
+s32 CONF_GetPadGuestDevices(conf_pad_guests *pads);
+s32 CONF_SetPadGuestDevices(const conf_pad_guests *pads);
 s32 CONF_GetNickName(u8 *nickname);
 s32 CONF_GetAspectRatio(void);
 s32 CONF_GetEULA(void);

gc/ogc/system.h
@@ -288,7 +288,7 @@ void SYS_ResetPMC(void);
 \brief Create/initialize sysalarm structure
 \param[in] thealarm pointer to the handle to store the created alarm context identifier
 
-\return 0 on succuess, non-zero on error
+\return 0 on success, non-zero on error
 */
 s32 SYS_CreateAlarm(syswd_t *thealarm);
 
@@ -299,7 +299,7 @@ s32 SYS_CreateAlarm(syswd_t *thealarm);
 \param[in] tp pointer to timespec structure holding the time to fire the alarm
 \param[in] cb pointer to callback which is called when the alarm fires.
 
-\return 0 on succuess, non-zero on error
+\return 0 on success, non-zero on error
 */
 s32 SYS_SetAlarm(syswd_t thealarm,const struct timespec *tp,alarmcallback cb,void *cbarg);
 
@@ -307,11 +307,11 @@ s32 SYS_SetAlarm(syswd_t thealarm,const struct timespec *tp,alarmcallback cb,voi
 /*! \fn s32 SYS_SetPeriodicAlarm(syswd_t thealarm,const struct timespec *tp_start,const struct timespec *tp_period,alarmcallback cb)
 \brief Set the alarm parameters for a periodioc alarm, add to the list of alarms and start. The alarm and interval persists as long as SYS_CancelAlarm() isn't called.
 \param[in] thealarm identifier to the alarm context to be initialized for a periodic alarm
-\param[in] tp_start pointer to timespec structure holding the time to fire first time the alarm
+\param[in] tp_start pointer to timespec structure holding the time to fire the alarm for first time
 \param[in] tp_period pointer to timespec structure holding the interval for all following alarm triggers.
 \param[in] cb pointer to callback which is called when the alarm fires.
 
-\return 0 on succuess, non-zero on error
+\return 0 on success, non-zero on error
 */
 s32 SYS_SetPeriodicAlarm(syswd_t thealarm,const struct timespec *tp_start,const struct timespec *tp_period,alarmcallback cb,void *cbarg);
 
@@ -320,15 +320,15 @@ s32 SYS_SetPeriodicAlarm(syswd_t thealarm,const struct timespec *tp_start,const 
 \brief Remove the given alarm context from the list of contexts and destroy it
 \param[in] thealarm identifier to the alarm context to be removed and destroyed
 
-\return 0 on succuess, non-zero on error
+\return 0 on success, non-zero on error
 */
 s32 SYS_RemoveAlarm(syswd_t thealarm);
 
 /*! \fn s32 SYS_CancelAlarm(syswd_t thealarm)
 \brief Cancel the alarm, but do not remove from the list of contexts.
-\param[in] thealarm identifier to the alram context to be canceled
+\param[in] thealarm identifier to the alarm context to be cancelled
 
-\return 0 on succuess, non-zero on error
+\return 0 on success, non-zero on error
 */
 s32 SYS_CancelAlarm(syswd_t thealarm);
 
@@ -393,6 +393,7 @@ void* SYS_GetArena2Hi(void);
 void SYS_SetArena2Hi(void *newHi);
 u32 SYS_GetArena2Size(void);
 powercallback SYS_SetPowerCallback(powercallback cb);
+void SYS_DoPowerCB(void);
 #endif
 
 /* \fn u64 SYS_Time(void)

gc/wiiuse/wiiuse.h
@@ -242,7 +242,8 @@ typedef enum cmd_blk_s
 {
 	CMD_READY = 0,
 	CMD_SENT,
-	CMD_DONE
+	CMD_DONE,
+	CMD_FAILED
 } cmd_blk_s;
 
 struct cmd_blk_t
@@ -660,9 +661,10 @@ typedef struct wiimote_t {
  * @brief Wiimote listen structure.
  */
 typedef struct wiimote_listen_t {
+	WCONST u8 name[0x40];
 	WCONST struct bd_addr bdaddr;
 	WCONST struct bte_pcb *sock;
-	WCONST struct wiimote_t *(*assign_cb)(struct bd_addr *bdaddr);
+	WCONST struct wiimote_t *(*assign_cb)(struct wiimote_listen_t *wml, u8 err);
 	WCONST struct wiimote_t *wm;
 } wiimote_listen;
 #endif
@@ -697,7 +699,8 @@ WIIUSE_EXPORT extern const char* wiiuse_version();
 #ifndef GEKKO
 WIIUSE_EXPORT extern struct wiimote_t** wiiuse_init(int wiimotes);
 #else
-WIIUSE_EXPORT extern int wiiuse_register(struct wiimote_listen_t *wml, struct bd_addr *bdaddr, struct wiimote_t *(*assign_cb)(struct bd_addr *bdaddr));
+WIIUSE_EXPORT extern int wiiuse_accept(struct wiimote_listen_t *wml, struct bd_addr *bdaddr, u8 *name, struct wiimote_t *(*assign_cb)(wiimote_listen *wml, u8 err));
+WIIUSE_EXPORT extern int wiiuse_connect(struct wiimote_listen_t *wml, struct bd_addr *bdaddr, u8 *name, struct wiimote_t *(*assign_cb)(wiimote_listen *wml, u8 err));
 WIIUSE_EXPORT extern struct wiimote_t** wiiuse_init(int wiimotes, wii_event_cb event_cb);
 WIIUSE_EXPORT extern void wiiuse_sensorbar_enable(int enable);
 #endif
@@ -721,7 +724,6 @@ WIIUSE_EXPORT extern int wiiuse_write_streamdata(struct wiimote_t *wm,ubyte *dat
 
 /* connect.c */
 WIIUSE_EXPORT extern int wiiuse_find(struct wiimote_t** wm, int max_wiimotes, int timeout);
-WIIUSE_EXPORT extern int wiiuse_connect(struct wiimote_t** wm, int wiimotes);
 WIIUSE_EXPORT extern void wiiuse_disconnect(struct wiimote_t* wm);
 
 /* events.c */

libogc/conf.c
@@ -1,9 +1,11 @@
 /*-------------------------------------------------------------
 
-conf.c -- SYSCONF support
+conf.c -- SYSCONF & setting.txt support
 
-Copyright (C) 2008
+Copyright (C) 2008-2025
 Hector Martin (marcan)
+tona
+Zarithya
 
 This software is provided 'as-is', without any express or implied
 warranty.  In no event will the authors be held liable for any
@@ -36,10 +38,13 @@ distribution.
 #include "processor.h"
 #include "conf.h"
 
+#define CONF_FILE_SIZE 0x4000
+#define CONF_TXT_FILE_SIZE 0x100
 
 static int __conf_inited = 0;
-static u8 __conf_buffer[0x4000] ATTRIBUTE_ALIGN(32);
-static char __conf_txt_buffer[0x101] ATTRIBUTE_ALIGN(32);
+static u8 __conf_buffer[CONF_FILE_SIZE] ATTRIBUTE_ALIGN(32);
+static char __conf_txt_buffer[CONF_TXT_FILE_SIZE + 1] ATTRIBUTE_ALIGN(32);
+static int __conf_buffer_dirty = false;
 
 static const char __conf_file[] ATTRIBUTE_ALIGN(32) = "/shared2/sys/SYSCONF";
 static const char __conf_txt_file[] ATTRIBUTE_ALIGN(32) = "/title/00000001/00000002/data/setting.txt";
@@ -49,7 +54,7 @@ void __CONF_DecryptTextBuffer(void)
 	u32 key = 0x73B5DBFA;
 	int i;
 	
-	for(i=0; i<0x100; i++) {
+	for(i=0; i<CONF_TXT_FILE_SIZE; i++) {
 		__conf_txt_buffer[i] ^= key & 0xff;
 		key = (key<<1) | (key>>31);
 	}
@@ -65,19 +70,19 @@ s32 CONF_Init(void)
 	fd = IOS_Open(__conf_file,1);
 	if(fd < 0) return fd;
 	
-	memset(__conf_buffer,0,0x4000);
-	memset(__conf_txt_buffer,0,0x101);
+	memset(__conf_buffer,0,CONF_FILE_SIZE);
+	memset(__conf_txt_buffer,0,CONF_TXT_FILE_SIZE + 1);
 	
-	ret = IOS_Read(fd, __conf_buffer, 0x4000);
+	ret = IOS_Read(fd, __conf_buffer, CONF_FILE_SIZE);
 	IOS_Close(fd);
-	if(ret != 0x4000) return CONF_EBADFILE;
+	if(ret != CONF_FILE_SIZE) return CONF_EBADFILE;
 	
 	fd = IOS_Open(__conf_txt_file,1);
 	if(fd < 0) return fd;
 	
-	ret = IOS_Read(fd, __conf_txt_buffer, 0x100);
+	ret = IOS_Read(fd, __conf_txt_buffer, CONF_TXT_FILE_SIZE);
 	IOS_Close(fd);
-	if(ret != 0x100) return CONF_EBADFILE;
+	if(ret != CONF_TXT_FILE_SIZE) return CONF_EBADFILE;
 	
 	if(memcmp(__conf_buffer, "SCv0", 4)) return CONF_EBADFILE;
 	
@@ -96,7 +101,7 @@ int __CONF_GetTxt(const char *name, char *buf, int length)
 	
 	if(!__conf_inited) return CONF_ENOTINIT;
 	
-	while(line < (__conf_txt_buffer+0x100) ) {
+	while(line < (__conf_txt_buffer+CONF_TXT_FILE_SIZE) ) {
 		delim = strchr(line, '=');
 		if(delim && ((delim - line) == nlen) && !memcmp(name, line, nlen)) {
 			delim++;
@@ -115,7 +120,7 @@ int __CONF_GetTxt(const char *name, char *buf, int length)
 		}
 		
 		// skip to line end
-		while(line < (__conf_txt_buffer+0x100) && *line++ != '\n');
+		while(line < (__conf_txt_buffer+CONF_TXT_FILE_SIZE) && *line++ != '\n');
 	}
 	return CONF_ENOENT;
 }
@@ -205,6 +210,74 @@ s32 CONF_Get(const char *name, void *buffer, u32 length)
 			return CONF_ENOTIMPL;
 	}
 	return len;
+}
+
+s32 CONF_Set(const char *name, const void *buffer, u32 length)
+{
+	u8 *entry;
+	s32 len;
+	if(!__conf_inited) return CONF_ENOTINIT;
+	
+	entry = __CONF_Find(name);
+	if(!entry) return CONF_ENOENT;
+	
+	len = CONF_GetLength(name);
+	if(len<0) return len;
+	if(len!=length) return CONF_EBADVALUE;
+	
+	switch(*entry>>5) {
+		case CONF_BIGARRAY:
+			memcpy(&entry[strlen(name)+3], buffer, len);
+			break;
+		case CONF_SMALLARRAY:
+			memcpy(&entry[strlen(name)+2], buffer, len);
+			break;
+		case CONF_BYTE:
+		case CONF_SHORT:
+		case CONF_LONG:
+		case CONF_BOOL:
+			memcpy(&entry[strlen(name)+1], buffer, len);
+			break;
+		default:
+			return CONF_ENOTIMPL;
+	}
+	__conf_buffer_dirty = true;
+	return len;
+}
+
+int __CONF_WriteBuffer(void)
+{
+	int ret, fd;
+
+	if (!__conf_inited)
+		return CONF_ENOTINIT;
+
+	if (!__conf_buffer_dirty)
+		return 0;
+
+	fd = IOS_Open(__conf_file, 2);
+	if (fd < 0)
+		return fd;
+
+	ret = IOS_Write(fd, __conf_buffer, CONF_FILE_SIZE);
+	IOS_Close(fd);
+	if (ret != CONF_FILE_SIZE)
+		return CONF_EBADFILE;
+
+	__conf_buffer_dirty = false;
+	return 0;
+}
+
+s32 CONF_SaveChanges(void)
+{
+	s32 ret;
+	if (!__conf_inited)
+		return CONF_ENOTINIT;
+	ret = __CONF_WriteBuffer();
+	if (ret < 0)
+		return ret;
+
+	return CONF_ERR_OK;
 }
 
 s32 CONF_GetShutdownMode(void) 
@@ -344,6 +417,37 @@ s32 CONF_GetPadDevices(conf_pads *pads)
 	if(res < 0) return res;
 	if(res < sizeof(conf_pads)) return CONF_EBADVALUE;
 	return 0;
+}
+
+s32 CONF_SetPadDevices(const conf_pads *pads)
+{
+	u8 count;
+
+	if (!pads) return CONF_EBADVALUE;
+	count = pads->num_registered;
+	if (count > CONF_PAD_MAX_REGISTERED) return CONF_EBADVALUE;
+	return CONF_Set("BT.DINF", pads, sizeof(conf_pads));
+}
+
+s32 CONF_GetPadGuestDevices(conf_pad_guests *pads)
+{
+	int res;
+	
+	res = CONF_Get("BT.CDIF", pads, sizeof(conf_pad_guests));
+	if(res < 0) return res;
+	if(res < sizeof(conf_pad_guests)) return CONF_EBADVALUE;
+	return 0;
+}
+
+s32 CONF_SetPadGuestDevices(const conf_pad_guests *pads)
+{
+	u8 count;
+	conf_pad_guests empty = {0};
+
+	if (!pads) pads = &empty;
+	count = pads->num_guests;
+	if (count > CONF_PAD_MAX_ACTIVE) return CONF_EBADVALUE;
+	return CONF_Set("BT.CDIF", pads, sizeof(conf_pad_guests));
 }
 
 s32 CONF_GetNickName(u8 *nickname)

libogc/system.c
@@ -156,6 +156,7 @@ static void __POWDefaultHandler(void);
 static powercallback __POWCallback = NULL;
 
 static u32 __sys_resetdown = 0;
+static u64 __sys_powerenabletime = 0;
 #endif
 
 static vu64* const _bootTime = (u64*)0x800030d8;
@@ -456,7 +457,7 @@ static void __STMEventHandler(u32 event)
 
 	if(event==STM_EVENT_POWER) {
 		_CPU_ISR_Disable(level);
-		__POWCallback();
+		SYS_DoPowerCB();
 		_CPU_ISR_Restore(level);
 	}
 }
@@ -1019,16 +1020,23 @@ void _V_EXPORTNAME(void)
 { __sys_versionbuild = _V_STRING; __sys_versiondate = _V_DATE_; }
 
 #if defined(HW_RVL)
-void __SYS_DoPowerCB(void)
+void SYS_DoPowerCB(void)
 {
 	u32 level;
-	powercallback powcb;
 
-	_CPU_ISR_Disable(level);
-	powcb = __POWCallback;
-	__POWCallback = __POWDefaultHandler;
-	powcb();
-	_CPU_ISR_Restore(level);
+	// Power button events fire repeatedly for one second after button is pressed
+	// Throttle events so only one callback gets called per button press
+	if(gettime() >= __sys_powerenabletime) {
+		_CPU_ISR_Disable(level);
+		__POWCallback();
+		_CPU_ISR_Restore(level);
+	}
+	__sys_powerenabletime = gettime() + millisecs_to_ticks(50);
+}
+
+void __SYS_DisablePowerButton(u32 disableTime)
+{
+	__sys_powerenabletime = gettime() + millisecs_to_ticks(disableTime);
 }
 #endif
 

commit 6641f4c77fad540045b0bbfb55ab2d92d8249cbb

Remove extraneous WPAD_CHAN_UNKNOWN field (fixes regression caused by ca84f9d7)

gc/wiiuse/wpad.h
@@ -43,7 +43,6 @@ enum {
 	WPAD_CHAN_2,
 	WPAD_CHAN_3,
 	WPAD_BALANCE_BOARD,
-	WPAD_CHAN_UNKNOWN,
 	WPAD_MAX_DEVICES,
 };
 